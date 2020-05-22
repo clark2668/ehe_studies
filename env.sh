ea409985f3298b5dbceeae5f558345112c2e56fe
clark2668 <baclark@msu.edu>
2020-05-22 15:31:08 -0500

    need working env

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+module purge
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/setup.sh`
+/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/env-shell.sh
