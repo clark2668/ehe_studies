901e5b0a8215f73c4355f6b89daa3bac33f920b7
clark2668 <baclark@msu.edu>
2020-05-26 11:44:50 -0500

    start docstrings now, or we never will...

--- ehe_utils.py
+++ ehe_utils.py
@@ -1,6 +1,18 @@
 from icecube import icetray
 
 def has_ehe_objects(frame):
+	"""
+	Return rue if a P-frame has EHE L2 objects.
+
+	This function will return true only if the p-frame that has been passed
+	contains the EHE L2 objects, namely the Ophelia and Portia objects
+
+	Parameters
+	----------
+	frame: I3 P-frame
+		An I3 P-frame to be checked
+
+	"""
 	passed_cut = False
 	passed_cut = (
 		frame.Has("EHEOpheliaParticleSRT_ImpLF")
