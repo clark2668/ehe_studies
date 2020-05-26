6fb49209243d1c832ff1132cf5ca1c2c2fda0ed6
clark2668 <baclark@msu.edu>
2020-05-26 09:38:41 -0500

    change from combo stable to a release just to do things properly

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 
 module purge
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/setup.sh`
-/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/stable/env-shell.sh
+/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/V00-00-03/env-shell.sh
