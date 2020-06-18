367d1a0ccbcd95624c592de687ea944854ef4e38
clark2668 <baclark@msu.edu>
2020-06-18 09:07:16 -0500

    make pyROOT available

--- env.sh
+++ env.sh
@@ -3,3 +3,4 @@
 module purge
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/setup.sh`
 /cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/V00-00-03/env-shell.sh
+export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
