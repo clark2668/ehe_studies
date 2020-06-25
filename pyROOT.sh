79824d0515ac61efe6ff55a5c0a7ebc87590d044
clark2668 <baclark@msu.edu>
2020-06-25 12:54:53 -0500

    put the EHE utils into the python path for easier use

--- pyROOT.sh
+++ pyROOT.sh
@@ -1,3 +1,4 @@
 # call this to get pyROOT
 #export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
 export PYTHONPATH=/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/lib:$PYTHONPATH
+export PYTHONPATH=/home/brianclark/IceCube/ehe_studies:$PYTHONPATH
