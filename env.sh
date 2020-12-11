d95642cf7ff0042955568025e9317122bc9c9857
clark2668 <baclark@msu.edu>
2020-12-11 08:01:18 -0600

    also need bump in env

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 module purge
-eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/setup.sh`
-/cvmfs/icecube.opensciencegrid.org/py3-v4.1.0/RHEL_7_x86_64/metaprojects/combo/V00-00-03/env-shell.sh
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
+/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/RHEL_7_x86_64/metaprojects/combo/V01-00-02/env-shell.sh
 #export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
