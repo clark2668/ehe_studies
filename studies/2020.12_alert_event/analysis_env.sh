226af88a6addc0acd579076e1598f0c22740aaad
clark2668 <baclark@msu.edu>
2021-03-01 13:10:10 -0600

    change what cvmfs we use

--- studies/2020.12_alert_event/analysis_env.sh
+++ studies/2020.12_alert_event/analysis_env.sh
@@ -1,4 +1,4 @@
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
 #$SROOT/metaprojects/combo/V01-00-02/env-shell.sh
-/data/user/brianclark/IceCube/ehe/modified_combo/bld/env-shell.sh
-
+#/data/user/brianclark/IceCube/ehe/modified_combo/bld/env-shell.sh
+/home/brianclark/IceCube/modified_icetray/build/env-shell.sh
