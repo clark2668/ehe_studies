80d968614e4a9c929aac015853a6a28dbf8abcc6
Brian Clark <baclark@msu.edu>
2021-04-02 06:22:01 +0900

    switch to custom combo

--- studies/2021.03_sc_studies/env.sh
+++ studies/2021.03_sc_studies/env.sh
@@ -1,3 +1,4 @@
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
 #/misc/home/brian/IceCube/modified_icetray/bld/env-shell.sh
-/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/RHEL_7_x86_64/metaprojects/combo/V01-01-00/env-shell.sh
+#/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/RHEL_7_x86_64/metaprojects/combo/V01-01-00/env-shell.sh
+/misc/home/brian/IceCube/modified_icetray/bld/env-shell.sh
