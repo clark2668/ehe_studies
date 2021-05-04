b8dca62e3f7c1472be1c83c7ac247d46ca438c39
Brian Clark <baclark@msu.edu>
2021-05-04 12:15:36 +0900

    add a version of the hqtot which excludes the CalibrationErrata

--- studies/2021.03_sc_studies/recalc_charge.py
+++ studies/2021.03_sc_studies/recalc_charge.py
@@ -37,16 +37,25 @@ scdata = '/misc/disk15/data/IceCube/RealData/86strings/standardcandle/2015/sc2/'
 tray.AddModule("I3Reader", filenamelist=[scdata+'Level2_IC86.2014_data_Run00125920_0116_0_138_GCD.i3.gz', 
 	args.input_files]) #GCD first
 
-pulse_name='SplitInIcePulses' # use SPLITInIcePulses
+pulse_name='SplitInIcePulses' # use *Split*InIcePulses
 
 tray.AddModule('HomogenizedQTot', 'qtot_total',
 	Pulses=pulse_name,
 	Output='HomogenziedQtot_SplitInIcePulses'
 	)
 
+tray.AddModule('HomogenizedQTot', 'qtot_total_caliberr',
+	Pulses=pulse_name,
+	Output='HomogenziedQtot_SplitInIcePulses_ExcludeCalibrationErrata',
+	ExcludeTimeWindows=True,
+	TimeWindowsToExclude=['CalibrationErrata']
+	)
+
+
 tray.AddSegment(hdfwriter.I3HDFWriter, 'hdf', 
 	Output="{}/y{}_c{}_f{}_hqtot_splitinnicepulses.hdf5".format(args.output_dir, args.year, args.candle, args.filter_setting), 
-	Keys=['I3EventHeader', 'HomogenizedQTot', 'HomogenziedQtot_SplitInIcePulses', 'EHEPortiaEventSummarySRT'], 
+	Keys=['I3EventHeader', 'HomogenizedQTot', 'HomogenziedQtot_SplitInIcePulses', 
+	'HomogenziedQtot_SplitInIcePulses_ExcludeCalibrationErrata', 'EHEPortiaEventSummarySRT'], 
 	SubEventStreams=['InIceSplit']
 	)
 
