be83bfcfc0a4c7ea83d31c3f2cd918d248583218
clark2668 <baclark@msu.edu>
2021-05-05 17:00:20 -0500

    start folder for simulation studies

---
+++ studies/2021.05_sim_studies/find_object.py
@@ -0,0 +1,46 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/icetray-start
+#METAPROJECT combo/V01-01-00
+
+from icecube import icetray, dataio, dataclasses
+from I3Tray import I3Tray
+from icecube import VHESelfVeto, hdfwriter
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", type=str,
+	dest="input_file",required=True,
+	help="full path to the input file",
+	)
+parser.add_argument("-o", type=str,
+	dest="output_file",required=True,
+	help='''full path to the output file, without file extention. 
+			That is, provide "test" not "test.i3.bz2"''',
+	)
+args = parser.parse_args()
+
+
+tray = I3Tray()
+tray.AddModule("I3Reader", filenamelist=[args.input_file]) #GCD first
+
+def has_needed(frame):
+	return frame.Has('EHEPortiaEventSummarySRT')
+
+# drop every frame that doesn't have the Portia information we need
+tray.AddModule(has_needed, 'something',
+	Streams=[icetray.I3Frame.Physics],
+	)
+
+tray.AddSegment(hdfwriter.I3HDFWriter, 'hdf', 
+	Output=f'{args.output_file}.hdf5', 
+	Keys=['I3EventHeader', 'Homogenized_QTot', 'EHEPortiaEventSummarySRT',
+	'I3MCTree_preMuonProp'], 
+	SubEventStreams=['InIceSplit']
+	)
+
+tray.AddModule("I3Writer", "write",
+	filename=f'{args.output_file}.i3.bz2',
+	Streams=[icetray.I3Frame.DAQ, icetray.I3Frame.Physics],
+	DropOrphanStreams=[icetray.I3Frame.Calibration, icetray.I3Frame.DAQ]
+	)
+
+tray.Execute()
