917c17ca6a2e4a539490d2a1d1c04048046c678e
Brian Clark <baclark@msu.edu>
2021-05-14 22:37:19 +0900

    tweak script to run the magsix study, instead of vanilla hqtot

--- studies/2021.03_sc_studies/scripts/run_hqtot_again/job.sh
+++ studies/2021.03_sc_studies/scripts/run_hqtot_again/job.sh
@@ -5,9 +5,16 @@ candle=$2
 filter=$3
 output_dir=$4
 
-files="/misc/disk19/users/brian/IceCube/standard_candle/2015/step3_addwave/y${year}_c${candle}_f${filter}_waves.i3.bz2"
+# original one
+# files="/misc/disk19/users/brian/IceCube/standard_candle/2015/step3_addwave/y${year}_c${candle}_f${filter}_waves.i3.bz2"
+#/misc/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies/recalc_charge.py -y $year -c $candle -f $filter -i $files -o $TMPDIR/
 
-/misc/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies/recalc_charge.py -y $year -c $candle -f $filter -i $files -o $TMPDIR/
+
+# new one, for mag six study (need my ehe_studies toolkit, so add to python path)
+export PYTHONPATH=/home/brian/IceCube/ehe_studies:$PYTHONPATH
+files="/misc/disk19/users/brian/IceCube/standard_candle/2015/step4_recalc_charge/y${year}_c${candle}_f${filter}_hqtot_splitinnicepulses.i3.bz2"
+/misc/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies/magsix_study/calc_magsix_charge.py -y $year -c $candle -f $filter -i $files -o $TMPDIR/
+
 
 mv $TMPDIR/*.i3.* $output_dir/.
 mv $TMPDIR/*hdf5 $output_dir/.
