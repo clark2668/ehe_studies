8d35380aef911d7a1c2cfeaf3b5f9f2602c6aa96
Brian Clark <baclark@msu.edu>
2021-05-16 00:30:30 +0900

    make code work with revised script for restoring waveforms

--- studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
+++ studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
@@ -4,9 +4,13 @@ input_file_dir=$1
 input_file_name=$2
 output_file_dir=$3
 output_file_name=$4
+exclude_atwd=$5
 
-script_path=/home/brianclark/IceCube/ehe_studies/studies/2021.05_sim_studies
+export PYTHONPATH=/home/brian/IceCube/ehe_studies:$PYTHONPATH
 
-$script_path/save_charge.py -i $input_file_dir/$input_file_name -o $TMPDIR/$output_file_name
+script_path=/home/brianclark/IceCube/ehe_studies/studies/2021.03_sc_studies
 
-mv $TMPDIR/*.hdf5 $output_file_dir/.
+$script_path/ restore_waveforms_v2.py -i $input_file_dir/$input_file_name -o $TMPDIR/$output_file_name --atwd $exclude_atwd
+
+mv $TMPDIR/*.i3* $output_file_dir/.
+mv $TMPDIR/*.hdf5* $output_file_dir/.
--- studies/2021.03_sc_studies/scripts/restore_waveforms/make_dag.py
+++ studies/2021.03_sc_studies/scripts/restore_waveforms/make_dag.py
@@ -2,40 +2,38 @@ import numpy as np
 import os
 from pathlib import Path
 
-output_directory_head = '/data/user/brianclark/IceCube/ehe/output/sim/'
+output_directory_head = '/home/brian/IceCube/sc_reprocess/v2/restore_waveforms/'
+exclude_atwd = True
 
-# datasets = [21218 (nue), 21220 (numu), 21221 (nutau), 21315 (muongun), 20787 (corsika)]
-datasets = [20787]
-for dataset in datasets:
 
-	dag_file_name = f'dagman_{dataset}.dag'
+dag_file_name = 'dagman_{}.dag'.format(exclude_atwd)
+instructions = ""
+instructions += 'CONFIG config.dagman\n'
+instructions += "\n\n"
+with open(dag_file_name, 'w') as f:
+	f.write(instructions)
+
+main_index = 0
+
+in_filelist = open(f'../list_2015_sc2_full.txt', 'r')
+for in_file in in_filelist.readlines():
+
+	# if main_index > 3:
+	# 	continue
+
+	in_file = in_file.rstrip('\n') # remove the newline
+	in_dir, in_file = os.path.split(in_file) # split between path and filename
+	in_file_noext = os.path.splitext(os.path.splitext(in_file)[0])[0] # strip twice (to get rid of .i3.zst/i3.bz2)
+	out_dir = output_directory_head
+	out_file = f'{in_file_noext}'
+
 	instructions = ""
-	instructions += 'CONFIG config.dagman\n'
-	instructions += "\n\n"
-	with open(dag_file_name, 'w') as f:
+	instructions += f'JOB job_{main_index} job.sub \n'
+	instructions += f'VARS job_{main_index} indir="{in_dir}" infile="{in_file}" outdir="{out_dir}" outfile="{out_file}" exludeATWD="{exclude_atwd}" \n\n'
+
+	with open(dag_file_name, 'a') as f:
 		f.write(instructions)
 
-	main_index = 0
+	main_index+=1
 
-	in_filelist = open(f'../list_{dataset}.txt', 'r')
-	for in_file in in_filelist.readlines():
-
-		# if main_index > 3:
-		# 	continue
-
-		in_file = in_file.rstrip('\n') # remove the newline
-		in_dir, in_file = os.path.split(in_file) # split between path and filename
-		in_file_noext = os.path.splitext(os.path.splitext(in_file)[0])[0] # strip twice (to get rid of .i3.zst)
-		out_dir = output_directory_head + f'{dataset}/'
-		out_file = f'{in_file_noext}'
-
-		instructions = ""
-		instructions += f'JOB job_{main_index} job.sub \n'
-		instructions += f'VARS job_{main_index} indir="{in_dir}" infile="{in_file}" outdir="{out_dir}" outfile="{out_file}" \n\n'
-
-		with open(dag_file_name, 'a') as f:
-			f.write(instructions)
-
-		main_index+=1
-
-	in_filelist.close()
+in_filelist.close()
