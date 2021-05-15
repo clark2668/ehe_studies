8c75950ef746713d304b55231e92bcbebe504d21
Brian Clark <baclark@msu.edu>
2021-05-15 23:39:32 +0900

    add v2 versions-- not working yet though

--- studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
+++ studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-year=$1
-candle=$2
-filter=$3
-output_dir=$4
+input_file_dir=$1
+input_file_name=$2
+output_file_dir=$3
+output_file_name=$4
 
-files=/misc/disk19/users/brian/IceCube/standard_candle/2015/step2_splitbyfilter/y${year}_c${candle}_f${filter}.i3.bz2
+script_path=/home/brianclark/IceCube/ehe_studies/studies/2021.05_sim_studies
 
-/misc/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies/restore_waveforms.py -y $year -c $candle -f $filter -i $files -o $TMPDIR/
+$script_path/save_charge.py -i $input_file_dir/$input_file_name -o $TMPDIR/$output_file_name
 
-mv $TMPDIR/*.i3.* $output_dir/.
-mv $TMPDIR/*hdf5 $output_dir/.
-
+mv $TMPDIR/*.hdf5 $output_file_dir/.
---
+++ studies/2021.03_sc_studies/scripts/restore_waveforms/make_dag.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+from pathlib import Path
+
+output_directory_head = '/data/user/brianclark/IceCube/ehe/output/sim/'
+
+# datasets = [21218 (nue), 21220 (numu), 21221 (nutau), 21315 (muongun), 20787 (corsika)]
+datasets = [20787]
+for dataset in datasets:
+
+	dag_file_name = f'dagman_{dataset}.dag'
+	instructions = ""
+	instructions += 'CONFIG config.dagman\n'
+	instructions += "\n\n"
+	with open(dag_file_name, 'w') as f:
+		f.write(instructions)
+
+	main_index = 0
+
+	in_filelist = open(f'../list_{dataset}.txt', 'r')
+	for in_file in in_filelist.readlines():
+
+		# if main_index > 3:
+		# 	continue
+
+		in_file = in_file.rstrip('\n') # remove the newline
+		in_dir, in_file = os.path.split(in_file) # split between path and filename
+		in_file_noext = os.path.splitext(os.path.splitext(in_file)[0])[0] # strip twice (to get rid of .i3.zst)
+		out_dir = output_directory_head + f'{dataset}/'
+		out_file = f'{in_file_noext}'
+
+		instructions = ""
+		instructions += f'JOB job_{main_index} job.sub \n'
+		instructions += f'VARS job_{main_index} indir="{in_dir}" infile="{in_file}" outdir="{out_dir}" outfile="{out_file}" \n\n'
+
+		with open(dag_file_name, 'a') as f:
+			f.write(instructions)
+
+		main_index+=1
+
+	in_filelist.close()
