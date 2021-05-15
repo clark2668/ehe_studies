af6818c418171364bfb1c3a4cce3d42eb1d2c041
Brian Clark <baclark@msu.edu>
2021-05-16 00:39:26 +0900

    fix yet more typos

--- studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
+++ studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
@@ -8,7 +8,7 @@ exclude_atwd=$5
 
 export PYTHONPATH=/home/brian/IceCube/ehe_studies:$PYTHONPATH
 
-script_path=/home/brianclark/IceCube/ehe_studies/studies/2021.03_sc_studies
+script_path=/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies
 
 $script_path/ restore_waveforms_v2.py -i $input_file_dir/$input_file_name -o $TMPDIR/$output_file_name --atwd $exclude_atwd
 
