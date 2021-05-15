af683530bef34d1f6e334f66b332f5c7ebc6a391
Brian Clark <baclark@msu.edu>
2021-05-16 00:42:46 +0900

    yet another typo, if you can believe it...

--- studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
+++ studies/2021.03_sc_studies/scripts/restore_waveforms/job.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=/home/brian/IceCube/ehe_studies:$PYTHONPATH
 
 script_path=/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies
 
-$script_path/restore_waveforms_v2.py -i $input_file_dir/$input_file_name -o $TMPDIR/$output_file_name --atwd $exclude_atwd
+$script_path/restore_waveforms_v2.py -i $input_file_dir/$input_file_name -o $TMPDIR/$output_file_name -atwd $exclude_atwd
 
 mv $TMPDIR/*.i3* $output_file_dir/.
 mv $TMPDIR/*.hdf5* $output_file_dir/.
