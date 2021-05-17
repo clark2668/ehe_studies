c1a077e5d534703571d4fe428b068a24df661660
Brian Clark <baclark@msu.edu>
2021-05-18 01:07:53 +0900

    adjust scripts, and remove matplotlib, which makes it unhappy for some reason

--- studies/2021.03_sc_studies/scripts/split_files/job.sh
+++ studies/2021.03_sc_studies/scripts/split_files/job.sh
@@ -4,9 +4,13 @@ year=$1
 candle=$2
 filter=$3
 output_dir=$4
+input_dir=$5
 
-files=/disk19/users/brian/IceCube/standard_candle/2015/w_hqtot/Level2_Run00125920_Subrun00000000_00000*.i3.bz2
+files=$input_dir/*.i3.*
+
+export PYTHONPATH=/home/brian/IceCube/ehe_studies:$PYTHONPATH
 
 /misc/home/brian/IceCube/ehe_studies/studies/2021.03_sc_studies/split_files.py -y $year -c $candle -f $filter -i $files -o $TMPDIR/
 
 mv $TMPDIR/*.i3.* $output_dir/.
+mv $TMPDIR/*.hdf5 $output_dir/.
