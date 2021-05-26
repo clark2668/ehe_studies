adc8dc88460dc2a5e92e93119d695baf73aa269d
Brian Clark <baclark@msu.edu>
2021-05-26 17:40:49 -0500

    update again

--- studies/2021.03_sc_studies/plot_ladder_result.py
+++ studies/2021.03_sc_studies/plot_ladder_result.py
@@ -14,49 +14,75 @@ data_nobnine = np.genfromtxt('ladder_results_nobnine.csv', delimiter=',',
 
 fig, axs = plt.subplots(1,1,figsize=(5,5))
 
-plot_style = 'absolute'
+
+
+plot_style = 'relative'
 if plot_style == 'absolute':
 	axs.plot(data_magsix['setting'], data_magsix['truth'], 'C7o', label='Truth')
 	
-	
-	# magsix plotting
-	# axs.plot(data['setting'], data['inhq'], 'C0-', marker='x', label='HQtot, Including ATWD')
-	# axs.plot(data['setting'], data['exhq'], 'C1--', marker='+', label='HQtot, Excluding ATWD')
-	# axs.plot(data['setting'], data['inpo'], 'C2-.', marker='1', label='Portia, Including ATWD')
-	# axs.plot(data['setting'], data['expo'], 'C3:', marker='2', label='Portia, Excluding ATWD')
 
-	axs.plot(data_magsix['setting'], data_magsix['hqtot_all'], 
-		'C0-', marker='x', label='HQtot, MagSix, FADC+ATWD')
+	# # mag six / noble nine comparison
+	# axs.plot(data_magsix['setting'], data_magsix['hqtot_all'], 
+	# 	'C0-', marker='x', label='HQtot, MagSix, FADC+ATWD')
+	# axs.plot(data_nobnine['setting'], data_nobnine['hqtot_all'], 
+	# 	'C1--', marker='+', label='HQtot, NobleNine, FADC+ATWD')
+	
+	# axs.plot(data_magsix['setting'], data_magsix['portia_all'], 
+	# 	'C2-.', marker='1', label='Portia, MagSix, FADC+ATWD')
+	# axs.plot(data_nobnine['setting'], data_nobnine['portia_all'], 
+	# 	'C3:', marker='2', label='Portia, NobleNine, FADC+ATWD')
+
+	# nobnine ATWD+FADC, ATWD, FADC Comparison
+	axs.set_title('Noble Nine')
 	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_all'], 
-		'C1--', marker='+', label='HQtot, NobleNine, FADC+ATWD')
-	
-	axs.plot(data_magsix['setting'], data_magsix['portia_all'], 
-		'C2-.', marker='1', label='Portia, MagSix, FADC+ATWD')
-	axs.plot(data_nobnine['setting'], data_nobnine['portia_all'], 
-		'C3:', marker='2', label='Portia, NobleNine, FADC+ATWD')
+		'C0-', marker='x', label='HQtot, FADC+ATWD')
+	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_fadc'], 
+		'C0-.', marker='+', label='HQtot, FADC')
+	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_atwd'], 
+		'C0--', marker='1', label='HQtot, ATWD')
 
-	# nobnine plotting
+
+	axs.plot(data_nobnine['setting'], data_nobnine['portia_all'], 
+		'C1-', marker='x', label='Portia, FADC+ATWD')
+	axs.plot(data_nobnine['setting'], data_nobnine['portia_fadc'], 
+		'C1-.', marker='+', label='Portia, FADC')
+	axs.plot(data_nobnine['setting'], data_nobnine['portia_atwd'], 
+		'C1--', marker='1', label='Portia, ATWD')
 
 	axs.set_ylabel('Filter Wheel Transmittivity')
 
 
 if plot_style=='relative':
 
-	# magsix plotting
-	# axs.plot(data['setting'], data['inhq']/data['truth'], 'C0-', marker='x', label='HQtot, Including ATWD')
-	# axs.plot(data['setting'], data['exhq']/data['truth'], 'C1--', marker='+', label='HQtot, Excluding ATWD')
-	# axs.plot(data['setting'], data['inpo']/data['truth'], 'C2-.', marker='1', label='Portia, Including ATWD')
-	# axs.plot(data['setting'], data['expo']/data['truth'], 'C3:', marker='2', label='Portia, Excluding ATWD')	
 
-	axs.plot(data_magsix['setting'], data_magsix['hqtot_all']/data_magsix['truth'], 
-		'C0-', marker='x', label='HQtot, MagSix, FADC+ATWD')
-	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_all']/data_nobnine['truth'], 
-		'C1--', marker='+', label='HQtot, NobleNine, FADC+ATWD')
+	# # mag six / noble nine comparison
+	# axs.plot(data_magsix['setting'], data_magsix['hqtot_all']/data_magsix['truth'], 
+	# 	'C0-', marker='x', label='HQtot, MagSix, FADC+ATWD')
+	# axs.plot(data_nobnine['setting'], data_nobnine['hqtot_all']/data_nobnine['truth'], 
+	# 	'C1--', marker='+', label='HQtot, NobleNine, FADC+ATWD')
 	
-	axs.plot(data_magsix['setting'], data_magsix['portia_all']/data_magsix['truth'], 
-		'C2-.', marker='1', label='Portia, MagSix, FADC+ATWD')
-	axs.plot(data_nobnine['setting'], data_nobnine['portia_all']/data_nobnine['truth'], 
-		'C3:', marker='2', label='Portia, NobleNine, FADC+ATWD')
+	# axs.plot(data_magsix['setting'], data_magsix['portia_all']/data_magsix['truth'], 
+	# 	'C2-.', marker='1', label='Portia, MagSix, FADC+ATWD')
+	# axs.plot(data_nobnine['setting'], data_nobnine['portia_all']/data_nobnine['truth'], 
+	# 	'C3:', marker='2', label='Portia, NobleNine, FADC+ATWD')
+
+
+	# nobnine ATWD+FADC, ATWD, FADC Comparison
+	axs.set_title('Noble Nine')
+	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_all']/data_magsix['truth'], 
+		'C0-', marker='x', label='HQtot, FADC+ATWD')
+	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_fadc']/data_magsix['truth'], 
+		'C0-.', marker='+', label='HQtot, FADC')
+	axs.plot(data_nobnine['setting'], data_nobnine['hqtot_atwd']/data_magsix['truth'], 
+		'C0--', marker='1', label='HQtot, ATWD')
+
+
+	axs.plot(data_nobnine['setting'], data_nobnine['portia_all']/data_magsix['truth'], 
+		'C1-', marker='x', label='Portia, FADC+ATWD')
+	axs.plot(data_nobnine['setting'], data_nobnine['portia_fadc']/data_magsix['truth'], 
+		'C1-.', marker='+', label='Portia, FADC')
+	axs.plot(data_nobnine['setting'], data_nobnine['portia_atwd']/data_magsix['truth'], 
+		'C1--', marker='1', label='Portia, ATWD')
 
 	axs.set_ylabel('Filter Wheel Transmittivity Measured/Predicted')
 
@@ -65,6 +91,6 @@ axs.set_xlabel('Setting')
 axs.set_xticks([2,3,4,5,6])
 axs.legend()
 plt.tight_layout()
-fig.savefig('ladder_plots/result_comparison_{}.png'.format(plot_style), dpi=300)
+fig.savefig('ladder_plots/result_comparison_{}_digi_compare.png'.format(plot_style), dpi=300)
 del fig, axs
 
