86796243f2762ae1b560aef32228eee65c8dfda8
clark2668 <baclark@msu.edu>
2021-07-07 13:51:11 -0500

    update which variables we are saving

--- studies/2021.05_sim_studies/save_charge.py
+++ studies/2021.05_sim_studies/save_charge.py
@@ -27,17 +27,19 @@ tray = I3Tray()
 tray.AddModule("I3Reader", filenamelist=[args.input_file]) #GCD first
 
 def has_needed(frame):
-	return frame.Has('EHEPortiaEventSummarySRT')
+	return frame.Has('EHEPortiaEventSummarySRT') and frame.Has('EHEOpheliaParticleSRT_ImpLF')
 
-# drop every frame that doesn't have the Portia information we need
+# drop every frame that doesn't have the information
 tray.AddModule(has_needed, 'something',
 	Streams=[icetray.I3Frame.Physics],
 	)
 
 tray.AddSegment(hdfwriter.I3HDFWriter, 'hdf', 
 	Output=f'{args.output_file}.hdf5', 
-	Keys=['I3EventHeader', 'Homogenized_QTot', 'EHEPortiaEventSummarySRT',
-	'I3MCTree_preMuonProp'], 
+	Keys=['I3MCWeightDict', 'I3EventHeader', 'I3MCTree_preMuonProp', 'PolyplopiaPrimary',
+    'I3PrimaryInjectorInfo', 'I3CorsikaWeight',
+    'Homogenized_QTot', 'LineFit', 
+    'EHEPortiaEventSummarySRT', 'EHEOpheliaParticleSRT_ImpLF'], 
 	SubEventStreams=['InIceSplit']
 	)
 
