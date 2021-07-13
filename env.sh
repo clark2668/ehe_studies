79b313f23dc2c0fd9b27cc2e6e1332b2bf03f20c
clark2668 <baclark@msu.edu>
2021-07-13 18:05:12 -0500

    make the env aware of what computer we're on when setting up the environment

--- env.sh
+++ env.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-module purge
-eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
-$SROOT/metaprojects/combo/V01-01-00/env-shell.sh
+hostname=$HOSTNAME
+
+if [[ $hostname == *"wisc"* ]]; then
+	echo 'At UW/WIPAC filesystem! (' $hostname ')'
+	export PYTHONPATH=/home/brianclark/IceCube/ehe_studies/utils/:$PYTHONPATH
+	export PYTHONPATH=/home/brianclark/IceCube/modified_icetray/pip_installed_tools/:$PYTHONPATH
+	export PYTHONPATH=/home/brianclark/IceCube/modified_icetray/simweights/:$PYTHONPATH
+fi
+#module purge
+#eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
+#$SROOT/metaprojects/combo/V01-01-00/env-shell.sh
 #/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/RHEL_7_x86_64/metaprojects/combo/V01-00-02/env-shell.sh
 #export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
