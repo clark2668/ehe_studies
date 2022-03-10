0710dd04fd57bbbea678a1ec130ac13964d0af35
clark2668 <baclark@msu.edu>
2022-03-10 09:37:25 -0600

    add some code to plot the L3 variables nicely

--- studies/2022.02_data_mc/plot_L3_comparison.py
+++ studies/2022.02_data_mc/plot_L3_comparison.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
 import argparse
+import utils_plot as up
 
 import utils_weights, utils_ehe
 livetime = 86400 * 365
@@ -43,7 +44,7 @@ elif which_one is 'new':
     zenith_label = "LineFit"
 
 atmo_model = 'H3a_SIBYLL23C'
-cr_model = 'GaisserH4a'
+cr_model = 'GaisserH3a'
 atmo_flux_model = utils_weights.get_flux_model(atmo_model, 'nugen')
 cr_flux_model = utils_weights.get_flux_model(cr_model, 'corsika')
 
@@ -51,17 +52,26 @@ numu_weighter = utils_weights.get_weighter(numu_file, 'nugen', 1000)
 nue_weighter = utils_weights.get_weighter(nue_file, 'nugen', 1000)
 cor_weighter = utils_weights.get_weighter(cor_file, 'corsika', 10000)
 
-# # numu_zenith = numu_weighter.get_column(zenith_var[0], zenith_var[1])
+charge_masks = {}
+log10_charge_cut = 4.4
+
+numu_zenith = numu_weighter.get_column(zenith_var[0], zenith_var[1])
+numu_coszenith = np.cos(numu_zenith)
 # # numu_chisqured = numu_weighter.get_column(fit_var[0], fit_var[1])
 numu_npe = numu_weighter.get_column(charge_var[0], charge_var[1])
+charge_masks['numu'] = np.log10(numu_npe)> log10_charge_cut
 
-# # nue_zenith = nue_weighter.get_column(zenith_var[0], zenith_var[1])
+nue_zenith = nue_weighter.get_column(zenith_var[0], zenith_var[1])
+nue_coszenith = np.cos(nue_zenith)
 # # nue_chisqured = nue_weighter.get_column(fit_var[0], fit_var[1])
 nue_npe = nue_weighter.get_column(charge_var[0], charge_var[1])
+charge_masks['nue'] = np.log10(nue_npe) > log10_charge_cut
 
-# # cor_zenith = cor_weighter.get_column(zenith_var[0], zenith_var[1])
+cor_zenith = cor_weighter.get_column(zenith_var[0], zenith_var[1])
+cor_coszenith = np.cos(cor_zenith)
 # # cor_chisqured = cor_weighter.get_column(fit_var[0], fit_var[1])
 cor_npe = cor_weighter.get_column(charge_var[0], charge_var[1])
+charge_masks['cor'] = np.log10(cor_npe)> log10_charge_cut
 
 numu_atmo_weights = numu_weighter.get_weights(atmo_flux_model)
 nue_atmo_weights = nue_weighter.get_weights(atmo_flux_model)
@@ -76,49 +86,229 @@ nue_atmo_weights *= livetime
 cor_weights *= livetime
 
 data_file = pd.HDFStore(args.data_file)
-values = np.asarray(data_file.get('Homogenized_QTot').get('value'))
 data_npe = np.asarray(data_file.get(charge_var[0]).get(charge_var[1]))
 data_npe = np.log10(data_npe)
+charge_masks['data'] = data_npe > log10_charge_cut
+data_zenith = np.asarray(data_file.get(zenith_var[0]).get(zenith_var[1]))
+data_coszenith = np.cos(data_zenith)
 
 do_L2_plot = True
 if do_L2_plot:
 
-    fig, (ax, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [2, 1]})
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    bins = np.linspace(3, 6, 40)
+    # this shows the 1D distributions of the L2 quantities
+    # namely the charge and zenith angle
+
+    #########################
+    #########################
+    ## Charge Histogram
+    #########################
+    #########################
+
+    fig, (ax, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [2, 1]})
+    bins = np.linspace(4.4, 6, 17)
 
     # histogram and then plot the data
-    data, data_bins = np.histogram(data_npe, bins=bins)
+    data, data_bins = np.histogram(data_npe[charge_masks['data']], bins=bins)
+    errs = np.sqrt(data)
+    binscenters = np.array([0.5 * (data_bins[i] + data_bins[i+1]) for i in range(len(data_bins)-1)])
+    ax.errorbar(binscenters, data, yerr=errs, fmt='ko', label='Burn Sample')
+
+    labels = [
+            r'Atm $\nu_{\mu}$' + ', {}'.format(atmo_model), 
+            r'Atm $\nu_{e}$'+ ', {}'.format(atmo_model), 
+            r'Atm $\mu$' + ', {}'.format(cr_model)
+            ]
+
+    # histogram the backgrounds
+    sim, sim_bins, patches = ax.hist(
+        [
+            np.log10(numu_npe)[charge_masks['numu']], 
+            np.log10(nue_npe)[charge_masks['nue']], 
+            np.log10(cor_npe)[charge_masks['cor']]
+        ],
+        weights=[
+            numu_atmo_weights[charge_masks['numu']], 
+            nue_atmo_weights[charge_masks['nue']], 
+            cor_weights[charge_masks['cor']]
+            ],
+        label=labels,
+        bins=bins,
+        stacked=True
+    )
+    ax.set_ylabel('Events in {:.2f} days'.format(livetime/60/60/24))
+    ax.set_yscale('log')
+    ax.set_ylim([1E-1, 1E4])
+    ax.legend()
+
+    # ratios
+    ax2.plot(binscenters, data/(sim[0]+sim[1]+sim[2]), 'ko', label='Sim/Data')
+    ax2.set_ylabel('Data/Sim')
+    ax2.set_xlabel('Charge ({}, NPE)'.format(charge_label))
+    ax2.plot([4, 6], [1, 1], 'k--') 
+    ax2.set_ylim([-1,2])
+
+    plt.tight_layout()
+    fig.savefig('L2_charge_hist_{}.png'.format(charge_label))
+
+    del fig, ax, ax2
+
+    #########################
+    #########################
+    ## Zenith Histogram
+    #########################
+    #########################
+
+
+    # now for cos(zen)
+    fig, (ax, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [2, 1]})
+    bins = np.linspace(-1, 1, 20)
+
+    # histogram and then plot the data
+    data, data_bins = np.histogram(data_coszenith[charge_masks['data']], bins=bins)
     errs = np.sqrt(data)
     binscenters = np.array([0.5 * (data_bins[i] + data_bins[i+1]) for i in range(len(data_bins)-1)])
     ax.errorbar(binscenters, data, yerr=errs, fmt='ko', label='Burn Sample')
 
     # histogram the backgrounds
     sim, sim_bins, patches = ax.hist(
-        [np.log10(numu_npe), np.log10(nue_npe), np.log10(cor_npe)],
-        weights=[numu_atmo_weights, nue_atmo_weights, cor_weights],
-        label=[
-            r'Atm $\nu_{\mu}$' + ', {}'.format(atmo_model), 
-            r'Atm $\nu_{e}$'+ ', {}'.format(atmo_model), 
-            r'Atm $\mu$' + ', {}'.format(cr_model)
+        [
+            numu_coszenith[charge_masks['numu']], 
+            nue_coszenith[charge_masks['nue']], 
+            cor_coszenith[charge_masks['cor']]
+        ],
+        weights=
+            [
+                numu_atmo_weights[charge_masks['numu']], 
+                nue_atmo_weights[charge_masks['nue']], 
+                cor_weights[charge_masks['cor']]
             ],
+        label=labels,
         bins=bins,
         stacked=True
     )
     ax.set_ylabel('Events in {:.2f} days'.format(livetime/60/60/24))
     ax.set_yscale('log')
-    ax.set_ylim([1E-1, 1E7])
-    ax.legend()
+    ax.set_ylim([1E-1, 1E4])
+    ax.legend(loc='upper left')
 
+    # ratios
     ax2.plot(binscenters, data/(sim[0]+sim[1]+sim[2]), 'ko', label='Sim/Data')
     ax2.set_ylabel('Data/Sim')
-    ax2.set_xlabel('Charge ({}, NPE)'.format(charge_label))
-    ax2.plot([3, 6], [1, 1], 'k--') 
+    ax2.set_xlabel(r'$cos \theta$ ({})'.format(zenith_label))
+    ax2.plot([-1.1, 1.1], [1, 1], 'k--') 
     ax2.set_ylim([-1,2])
 
     plt.tight_layout()
-    fig.savefig('burn_sample.png')
+    fig.savefig('L2_coszenith_hist_{}.png'.format(zenith_label))
+    del fig, ax, ax2
+
+    #########################
+    #########################
+    ## 2D HQtot vs Zenith histograms
+    #########################
+    #########################
+    labels = {
+        'x': r'$cos \theta$ ({})'.format(zenith_label),
+        'y': r'log$_{10}$(Charge)' + "({}, NPE)".format(charge_label)
+        }
+
+    lims = 1E-3, 1E3
+
+    # the ax order looks moronic, but the panels make more sense later
+    fig, ((ax3, ax, ax2), (ax5, ax6, ax4)) = plt.subplots(2, 3, 
+        gridspec_kw={
+            'height_ratios': [1, 1],
+            'width_ratios': [1, 1, 1]},
+        figsize=(15,10)
+        )
+    bins = [np.linspace(-1,1,20), np.linspace(4.4, 6, 17)]
+    my_map = plt.cm.plasma
+
+    # Corsika
+    # sim_cor, sim_cor_xedges, sim_cor_yedges, sim_cor_im = ax.hist2d(
+    #     cor_coszenith, np.log10(cor_npe), weights=cor_weights,
+    #     bins=bins,
+    #     cmap=my_map,
+    #     norm=colors.LogNorm(),
+    #     cmin=1E-3
+    # )
+    # ax.set_title(r'MC: $\mu$ '+ f'{cr_model}')
+    # ax.set_ylabel(r'log$_{10}$(Charge)' + "({}, NPE)".format(charge_label))
+    # ax.set_xlabel(r'$cos \theta$ ({})'.format(zenith_label))
+    # sim_cor_cbar = plt.colorbar(sim_cor_im, ax=ax)
+    # sim_cor_im.set_clim(lims)
+
+    # corsika
+    sim_cor, sim_cor_edges, sim_cor_yedges, sim_cor_im = up.make_2D_hist(
+        ax, cor_coszenith, np.log10(cor_npe), cor_weights,
+        bins, my_map, colors.LogNorm(), 1E-3,
+        labels['x'], labels['y'],
+        r'MC: $\mu$ '+ f'{cr_model}'   
+    )
+    sim_cor_cbar = plt.colorbar(sim_cor_im, ax=ax)
+    sim_cor_im.set_clim(lims)
+
+    # atmospheric neutrinos
+    sim_atmo, sim_atmo_xedges, sim_atmo_yedges, sim_atmo_im = up.make_2D_hist(
+        ax2, 
+        np.concatenate([numu_coszenith, nue_coszenith]), 
+        np.concatenate([np.log10(numu_npe), np.log10(nue_npe)]),
+        np.concatenate([numu_atmo_weights, nue_atmo_weights]),
+        bins, my_map, colors.LogNorm(), 1E-3,
+        labels['x'], labels['y'],
+        r'atm $\nu_{\mu} + \nu_{e}$' + f'{atmo_model}'   
+    )
+    sim_atmo_cbar = plt.colorbar(sim_atmo_im, ax=ax2)
+    sim_atmo_im.set_clim(lims)
+
+    # sum over all MC
+    sim, sim_xedges, sim_yedges, sim_im = up.make_2D_hist(
+        ax3, 
+        np.concatenate([numu_coszenith, nue_coszenith, cor_coszenith]), 
+        np.concatenate([np.log10(numu_npe), np.log10(nue_npe), np.log10(cor_npe)]),
+        np.concatenate([numu_atmo_weights, nue_atmo_weights, cor_weights]),
+        bins, my_map, colors.LogNorm(), 1E-3,
+        labels['x'], labels['y'],
+        r'MC: $\mu$ '+ f'{cr_model}' + r', atm $\nu_{\mu} + \nu_{e}$' + f'{atmo_model}'
+    )
+    sim_cbar = plt.colorbar(sim_im, ax=ax3)
+    sim_im.set_clim(lims)
+
+    # burn sample
+    data, data_xedges, data_yedges, data_im = up.make_2D_hist(
+        ax5, 
+        data_coszenith, 
+        data_npe,
+        np.ones_like(data_npe),
+        bins, my_map, colors.LogNorm(), 1E-3,
+        labels['x'], labels['y'],
+        "Data: Burn Sample, {:.2f} days".format(livetime/60/60/24)
+    )
+    data_cbar = plt.colorbar(data_im, ax=ax5)
+    data_im.set_clim(lims)
+
+    ratio = data/sim
+    ratio_im = ax6.pcolorfast(data_xedges, data_yedges, ratio.T,
+        cmap=plt.cm.plasma,
+        # vmin=-1,
+        # vmax=1,
+        # norm = up.CenteredNorm(vcenter=1, halfrange=0.5)
+        # norm=up.MidpointNormalize(
+        #     midpoint=1,
+        #     vmin=-1, 
+        #     vmax=1,
+        # )
+    )
+    ratio_cbar = plt.colorbar(ratio_im, ax=ax6)
+    ax6.set_title('Data/Sim')
+    ax6.set_xlabel(labels['x'])
+    ax6.set_ylabel(labels['y'])
+    ratio_im.set_clim(-1, 2)
+
+    fig.tight_layout()
+    fig.savefig('L2_charge_vs_coszen_{}_{}.png'.format(charge_label, zenith_label))
+
+
 
 data_file.close()
--- utils/utils_plot.py
+++ utils/utils_plot.py
@@ -1,4 +1,8 @@
 import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
 
 def set_axis_labels(ax, xlabel, ylabel, label_sizer, tick_sizer):
 
@@ -51,3 +55,127 @@ def find_contours_2D(x_values,y_values,xbins,weights=None,c1=16,c2=84):
 
     # the first return with the [1:] and [:-1] is about locating the bin centers
     return numpy.asarray((xbins[1:] + xbins[:-1])/2), numpy.asarray(median), numpy.asarray(lower), numpy.asarray(upper)
+
+def make_2D_hist(
+    ax, xvals, yvals, weights, bins, 
+    cmap=plt.cm.plasma, norm=None, cmin=None,
+    xlabel=None, ylabel=None, title=None,
+):
+
+    vals, xedges, yedges, im = ax.hist2d(
+        xvals, yvals, weights=weights,
+        bins=bins, cmap=cmap, norm=norm, cmin=cmin
+    )
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    return vals, xedges, yedges, im
+
+# set the colormap and centre the colorbar
+class MidpointNormalize(colors.Normalize):
+	"""
+	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+	"""
+	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+		self.midpoint = midpoint
+		colors.Normalize.__init__(self, vmin, vmax, clip)
+
+	def __call__(self, value, clip=None):
+		# I'm ignoring masked values and all kinds of edge cases to make a
+		# simple example...
+		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+
+
+class CenteredNorm(colors.Normalize):
+    def __init__(self, vcenter=0, halfrange=None, clip=False):
+        """
+        Normalize symmetrical data around a center (0 by default).
+        Unlike `TwoSlopeNorm`, `CenteredNorm` applies an equal rate of change
+        around the center.
+        Useful when mapping symmetrical data around a conceptual center
+        e.g., data that range from -2 to 4, with 0 as the midpoint, and
+        with equal rates of change around that midpoint.
+        Parameters
+        ----------
+        vcenter : float, default: 0
+            The data value that defines ``0.5`` in the normalization.
+        halfrange : float, optional
+            The range of data values that defines a range of ``0.5`` in the
+            normalization, so that *vcenter* - *halfrange* is ``0.0`` and
+            *vcenter* + *halfrange* is ``1.0`` in the normalization.
+            Defaults to the largest absolute difference to *vcenter* for
+            the values in the dataset.
+        Examples
+        --------
+        This maps data values -2 to 0.25, 0 to 0.5, and 4 to 1.0
+        (assuming equal rates of change above and below 0.0):
+            >>> import matplotlib.colors as mcolors
+            >>> norm = mcolors.CenteredNorm(halfrange=4.0)
+            >>> data = [-2., 0., 4.]
+            >>> norm(data)
+            array([0.25, 0.5 , 1.  ])
+        """
+        super().__init__(vmin=None, vmax=None, clip=clip)
+        self._vcenter = vcenter
+        # calling the halfrange setter to set vmin and vmax
+        self.halfrange = halfrange
+
+    def _set_vmin_vmax(self):
+        """
+        Set *vmin* and *vmax* based on *vcenter* and *halfrange*.
+        """
+        self.vmax = self._vcenter + self._halfrange
+        self.vmin = self._vcenter - self._halfrange
+
+    def autoscale(self, A):
+        """
+        Set *halfrange* to ``max(abs(A-vcenter))``, then set *vmin* and *vmax*.
+        """
+        A = np.asanyarray(A)
+        self._halfrange = max(self._vcenter-A.min(),
+                              A.max()-self._vcenter)
+        self._set_vmin_vmax()
+
+    def autoscale_None(self, A):
+        """Set *vmin* and *vmax*."""
+        A = np.asanyarray(A)
+        if self._halfrange is None and A.size:
+            self.autoscale(A)
+
+    @property
+    def vcenter(self):
+        return self._vcenter
+
+    @vcenter.setter
+    def vcenter(self, vcenter):
+        if vcenter != self._vcenter:
+            self._vcenter = vcenter
+            self._changed()
+        if self.vmax is not None:
+            # recompute halfrange assuming vmin and vmax represent
+            # min and max of data
+            self._halfrange = max(self._vcenter-self.vmin,
+                                  self.vmax-self._vcenter)
+            self._set_vmin_vmax()
+
+    @property
+    def halfrange(self):
+        return self._halfrange
+
+    @halfrange.setter
+    def halfrange(self, halfrange):
+        if halfrange is None:
+            self._halfrange = None
+            self.vmin = None
+            self.vmax = None
+        else:
+            self._halfrange = abs(halfrange)
+
+    def __call__(self, value, clip=None):
+        if self._halfrange is not None:
+            # enforce symmetry, reset vmin and vmax
+            self._set_vmin_vmax()
+        return super().__call__(value, clip=clip)
