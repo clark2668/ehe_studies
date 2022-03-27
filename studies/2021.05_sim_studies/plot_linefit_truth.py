4786da6ed6aa321d8c3a3b62e198187ce49438fe
clark2668 <baclark@msu.edu>
2022-03-27 14:24:18 -0500

    update code to make vs depth plots for linefit truth

--- studies/2021.05_sim_studies/plot_linefit_truth.py
+++ studies/2021.05_sim_studies/plot_linefit_truth.py
@@ -24,6 +24,7 @@ nue_file = pd.HDFStore(args.nue_file)
 cor_file = pd.HDFStore(args.cor_file)
 
 which_one = 'new'
+cog_var = ['HitStatisticsValues', 'cog_z']
 if which_one is 'original':
     charge_var = ['EHEPortiaEventSummarySRT', 'bestNPEbtw']
     zenith_var = ['EHEOpheliaParticleSRT_ImpLF', 'zenith']
@@ -33,8 +34,8 @@ if which_one is 'original':
     zenith_label = "Ophelia"
 elif which_one is 'new':
     charge_var = ['Homogenized_QTot', 'value']
-    zenith_var = ['LineFit', 'zenith']
-    speed_var = ['LineFit', 'speed']
+    zenith_var = ['LineFit_redo', 'zenith']
+    speed_var = ['LineFit_redo', 'speed']
     zenith_var_truth = ['PolyplopiaPrimary', 'zenith']
     charge_label = "HQtot"
     zenith_label = "LineFit"
@@ -50,18 +51,21 @@ numu_zenith = numu_weighter.get_column(zenith_var[0], zenith_var[1])
 numu_zenith_truth = numu_weighter.get_column(zenith_var_truth[0], zenith_var_truth[1])
 numu_npe = numu_weighter.get_column(charge_var[0], charge_var[1])
 numu_speed = numu_weighter.get_column(speed_var[0], speed_var[1])
+numu_inttypes = numu_weighter.get_column('I3MCWeightDict', 'InteractionType')
+numu_cog = numu_weighter.get_column(cog_var[0], cog_var[1])
 
 nue_zenith = nue_weighter.get_column(zenith_var[0], zenith_var[1])
 nue_zenith_truth = nue_weighter.get_column(zenith_var_truth[0], zenith_var_truth[1])
 nue_npe = nue_weighter.get_column(charge_var[0], charge_var[1])
 nue_speed = nue_weighter.get_column(speed_var[0], speed_var[1])
+nue_cog = nue_weighter.get_column(cog_var[0], cog_var[1])
 
-# cor_zenith = cor_weighter.get_column(zenith_var[0], zenith_var[1])
-cor_zenith = cor_weighter.get_column('LineFit_repeat', 'zenith')
+cor_zenith = cor_weighter.get_column(zenith_var[0], zenith_var[1])
 cor_zenith_truth = cor_weighter.get_column(zenith_var_truth[0], zenith_var_truth[1])
 cor_npe = cor_weighter.get_column(charge_var[0], charge_var[1])
-# cor_speed = cor_weighter.get_column(speed_var[0], speed_var[1])
-cor_speed = cor_weighter.get_column('LineFit_repeat', 'speed')
+cor_speed = cor_weighter.get_column(speed_var[0], speed_var[1])
+cor_cog = cor_weighter.get_column(cog_var[0], cog_var[1])
+
 
 numu_atmo_weights = numu_weighter.get_weights(atmo_flux_model)
 nue_atmo_weights = nue_weighter.get_weights(atmo_flux_model)
@@ -78,12 +82,13 @@ cor_weights *= livetime
 cmap=plt.cm.viridis
 sizer=20
 
-do_npe_chisqured_cut = True
-if do_npe_chisqured_cut:
+numu_npe_mask = numu_npe > 25000
+nue_npe_mask = nue_npe > 25000
+cor_npe_mask = cor_npe > 25000
 
-    numu_npe_mask = numu_npe > 25000
-    nue_npe_mask = nue_npe > 25000
-    cor_npe_mask = cor_npe > 25000
+
+do_speed_vs_zenith = False
+if do_speed_vs_zenith:
 
     # 2D hist, linefit speed vs true direction
 
@@ -150,4 +155,75 @@ if do_npe_chisqured_cut:
 
     plt.tight_layout()
     fig.savefig('plots/speed_vs_truezenith_{}.png'.format(which_one), edgecolor='none', bbox_inches='tight', dpi=300)
+    del fig, ax2, ax3
+
+do_speed_vs_cog = True
+if do_speed_vs_cog:
+
+    # 2D hist, linefit speed vs cog
+    numu_nc_mask = numu_inttypes > 0
+
+    fig = plt.figure(figsize=(27,7))
+    bins = [ np.linspace(-600,600,51), np.linspace(0,.5,51)]
+    
+    # corsika
+    ax = fig.add_subplot(131)
+    counts, xedges, yedges, im = ax.hist2d(
+            cor_cog[cor_npe_mask],
+            cor_speed[cor_npe_mask],
+            bins=bins,
+            weights=cor_weights[cor_npe_mask],
+            cmap=cmap,
+            norm=colors.LogNorm(),
+        )
+    # im.set_clim(1E-5, 1E3)
+    max_count = np.max(counts)
+    cbar = plt.colorbar(im, ax=ax)
+    cbar.set_label('Events/Year', fontsize=sizer)
+    cbar.ax.tick_params(labelsize=sizer) 
+    ax.set_xlabel(r'Center of Gravity Z', fontsize=sizer)
+    ax.set_ylabel('LineFit Speed', fontsize=sizer)
+    ax.tick_params(labelsize=sizer)
+    ax.set_title('Corsika (H3a)', fontsize=sizer)
+
+    # numu
+    ax2 = fig.add_subplot(132)
+    counts, xedges, yedges, im = ax2.hist2d(
+            numu_cog[numu_npe_mask & numu_nc_mask],
+            numu_speed[numu_npe_mask & numu_nc_mask],
+            bins=bins,
+            weights=numu_atmo_weights[numu_npe_mask & numu_nc_mask],
+            cmap=cmap,
+            norm=colors.LogNorm(),
+        )
+    # im.set_clim(1E-5, 1E3)
+    cbar2 = plt.colorbar(im, ax=ax2)
+    cbar2.set_label('Events/Year', fontsize=sizer)
+    cbar2.ax.tick_params(labelsize=sizer) 
+    ax2.set_xlabel(r'Center of Gravity Z', fontsize=sizer)
+    ax2.set_ylabel('LineFit Speed', fontsize=sizer)
+    ax2.tick_params(labelsize=sizer)
+    ax2.set_title('NuMu (H3a+SIBYLL23C)', fontsize=sizer)
+
+    # nue
+    ax3 = fig.add_subplot(133)
+    counts, xedges, yedges, im = ax3.hist2d(
+            nue_cog[nue_npe_mask],
+            nue_speed[nue_npe_mask],
+            bins=bins,
+            weights=nue_atmo_weights[nue_npe_mask],
+            cmap=cmap,
+            norm=colors.LogNorm(),
+        )
+    # im.set_clim(1E-5, 1E3)
+    cbar3 = plt.colorbar(im, ax=ax3)
+    cbar3.set_label('Events/Year', fontsize=sizer)
+    cbar3.ax.tick_params(labelsize=sizer) 
+    ax3.set_xlabel(r'Center of Gravity Z', fontsize=sizer)
+    ax3.set_ylabel('LineFit Speed', fontsize=sizer)
+    ax3.tick_params(labelsize=sizer)
+    ax3.set_title('NuE (H3a+SIBYLL23C)', fontsize=sizer)
+
+    plt.tight_layout()
+    fig.savefig('plots/speed_vs_cogz_{}.png'.format(which_one), edgecolor='none', bbox_inches='tight', dpi=300)
     del fig, ax2, ax3
