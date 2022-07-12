f2c4a841b205941ed99fce110c597a8de3921bcd
Brian Clark <baclark@msu.edu>
2022-07-12 17:59:56 +0900

    add some weighting functions for getting weights vs Enu

---
+++ other/juliet_weighting/weight_juliet_v2.py
@@ -0,0 +1,55 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import pandas as pd
+import tables
+import weights as weighter
+
+
+n_gen = 10000 * 150
+livetime = 86400 * 365 # 1 year
+
+# he_file = 'juliet_weight_test.hd5'
+# he_file = 'juliet_weight_test_vhe.hd5'
+# he_file = '/home/mmeier/data/simulations/table_based_sim/juliet/mu/high_energy/l2_prime/1/Level2_prime_00000001.hd5'
+# he_file = '/home/mmeier/data/simulations/table_based_sim/juliet/mu/high_energy/l2_prime/mu_high_energy_l2_prime_1k_merged.hd5'
+he_file = "/home/mmeier/data/simulations/table_based_sim/juliet/mu/high_energy/l2_prime/1/Level2_prime_00000001.hd5"
+
+with tables.open_file(he_file) as f:
+
+    primary = f.get_node('/I3JulietPrimaryParticle')
+    energies = primary.col('energy')
+
+    weight_dict = f.get_node('/JulietWeightDict')
+    prop_matrix = f.get_node('/PropagationMatrixNuTau').col('item').reshape(-1,140)
+    prop_matrix += f.get_node('/PropagationMatrixNuMu').col('item').reshape(-1,140)
+    prop_matrix += f.get_node('/PropagationMatrixNuE').col('item').reshape(-1,140)
+
+    # get weights by calculating them ourselves
+    def sample_flux(e_in_gev):
+        return 1E-8 * (e_in_gev ** -2.)
+    
+    energy_bins, weights = weighter.calc_juliet_flux_weight(
+        weight_dict = weight_dict,
+        prop_matrix = prop_matrix,
+        flux = sample_flux,
+        n_gen = n_gen,
+        livetime = livetime
+    )
+    bin_centers = (energy_bins[1:] + energy_bins[:-1]) * 0.5
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    evs1,_ ,__ = ax.hist(
+        bin_centers, 
+        bins=energy_bins, 
+        weights=weights, histtype='step'
+        )
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Neutrino Energy log10(GeV)')
+    ax.set_ylabel('Weighted Counts/Year')
+    plt.tight_layout()
+    fig.savefig('events_vs_enu.png')
+    del fig, ax
--- other/juliet_weighting/weights.py
+++ other/juliet_weighting/weights.py
@@ -110,3 +110,55 @@ def calc_juliet_weight_from_weight_dict_and_prop_matrix(
     weight = (injection_area * sqm2sqcm * omega_gen *
               livetime * int_w * flux_weight / n_gen_per_dec)
     return weight
+
+
+def calc_juliet_flux_weight(
+    weight_dict,
+    prop_matrix,
+    flux,
+    n_gen,
+    livetime=1
+):
+
+    energy_bins = np.logspace(5, 12, 141)
+    bin_center = (energy_bins[1:] + energy_bins[:-1]) * 0.5
+
+    # get interaction weights
+    int_w = weight_dict.col('InteractionWeightW')
+    int_w[int_w > 1] = 0 # eliminate weights > 1 # TODO: Why does this happen?
+
+    # weight the propagation matrix by the interaction probability
+    # NxM (N events x M energy bins) times list of length N (N events)
+    prop_matrix *= int_w[:, np.newaxis] # 
+    
+    # sum row-wise to get sum of prop matrices.
+    # result is array of length 140
+    # describing total weight (summed across events)
+    # in that energy bin
+    summed_prop_weight_per_E_bin = np.sum(prop_matrix, axis=0)
+
+    # evaluate the fluxes at the M energy bins
+    flux_values = flux(bin_center) * bin_center * np.log(10)
+
+    # Calculate the generation solid angle
+    cos_th_min = weight_dict.col('MinimumCosTheta')[0]
+    cos_th_max = weight_dict.col('MaximumCosTheta')[0]
+    phi_min = weight_dict.col('MinimumPhi')[0]
+    phi_max = weight_dict.col('MaximumPhi')[0]
+    omega_gen = (phi_max - phi_min) * (cos_th_max - cos_th_min)
+
+    sqm2sqcm = 10000
+    log_e_min = weight_dict.col('MinimumEnergyLog')[0]
+    log_e_max = weight_dict.col('MaximumEnergyLog')[0]
+    n_gen_per_dec = n_gen / (log_e_max - log_e_min)
+
+    r = weight_dict.col('InjectionSurfaceR')[0]
+    injection_area = np.pi * (r**2.)
+
+
+    # this a list of length  M
+    weight = (summed_prop_weight_per_E_bin * flux_values *
+        (injection_area * sqm2sqcm * omega_gen * # scaling factors
+        livetime / n_gen_per_dec)
+    )
+    return energy_bins, weight
