655ac5877041541e47b0b4b674d82bbaa8b61a24
Brian Clark <baclark@msu.edu>
2022-07-13 17:17:40 +0900

    update with charge, but don't really need it

--- other/juliet_weighting/weight_juliet_v2.py
+++ other/juliet_weighting/weight_juliet_v2.py
@@ -20,40 +20,41 @@ with tables.open_file(he_file) as f:
 
     charge = f.get_node('/Homogenized_QTot')
     charge = charge.col('value')
-    print(charge)
+    qbins = np.linspace(2,7,21)
+
+    primary = f.get_node('/I3JulietPrimaryParticle')
+    energies = primary.col('energy')
+
+    # accumulate the prop matrix
+    weight_dict = f.get_node('/JulietWeightDict')
+    prop_matrix = f.get_node('/PropagationMatrixNuTau').col('item').reshape(-1,140)
+    prop_matrix += f.get_node('/PropagationMatrixNuMu').col('item').reshape(-1,140)
+    prop_matrix += f.get_node('/PropagationMatrixNuE').col('item').reshape(-1,140)
+
+    # get weights by calculating them ourselves
+    def sample_flux(e_in_gev):
+        return 1E-8 * (e_in_gev ** -2.)
     
-    # primary = f.get_node('/I3JulietPrimaryParticle')
-    # energies = primary.col('energy')
+    energy_bins, weights = weighter.calc_juliet_flux_weight(
+        weight_dict = weight_dict,
+        prop_matrix = prop_matrix,
+        flux = sample_flux,
+        n_gen = n_gen,
+        livetime = livetime
+    )
+    bin_centers = (energy_bins[1:] + energy_bins[:-1]) * 0.5
 
-    # weight_dict = f.get_node('/JulietWeightDict')
-    # prop_matrix = f.get_node('/PropagationMatrixNuTau').col('item').reshape(-1,140)
-    # prop_matrix += f.get_node('/PropagationMatrixNuMu').col('item').reshape(-1,140)
-    # prop_matrix += f.get_node('/PropagationMatrixNuE').col('item').reshape(-1,140)
-
-    # # get weights by calculating them ourselves
-    # def sample_flux(e_in_gev):
-    #     return 1E-8 * (e_in_gev ** -2.)
-    
-    # energy_bins, weights = weighter.calc_juliet_flux_weight(
-    #     weight_dict = weight_dict,
-    #     prop_matrix = prop_matrix,
-    #     flux = sample_flux,
-    #     n_gen = n_gen,
-    #     livetime = livetime
-    # )
-    # bin_centers = (energy_bins[1:] + energy_bins[:-1]) * 0.5
-
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # evs1,_ ,__ = ax.hist(
-    #     bin_centers, 
-    #     bins=energy_bins, 
-    #     weights=weights, histtype='step'
-    #     )
-    # ax.set_xscale('log')
-    # ax.set_yscale('log')
-    # ax.set_xlabel('Neutrino Energy log10(GeV)')
-    # ax.set_ylabel('Weighted Counts/Year')
-    # plt.tight_layout()
-    # fig.savefig('events_vs_enu.png')
-    # del fig, ax
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    evs1,_ ,__ = ax.hist(
+        bin_centers, 
+        bins=energy_bins, 
+        weights=weights, histtype='step'
+        )
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Neutrino Energy log10(GeV)')
+    ax.set_ylabel('Weighted Counts/Year')
+    plt.tight_layout()
+    fig.savefig('events_vs_enu.png')
+    del fig, ax
