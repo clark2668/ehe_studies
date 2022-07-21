2da1628784d0df589448c1e1c93fc13ad5b7c321
clark2668 <baclark@msu.edu>
2022-07-21 01:22:27 -0500

    find some additional events

--- studies/2022.02_data_mc/find_outlier/find_event.py
+++ studies/2022.02_data_mc/find_outlier/find_event.py
@@ -13,34 +13,42 @@ def find_event(frame, run, evt):
             # 		keeper=False
     return keeper
 
-tray = I3Tray()
-# run_no = 78700221
-# evt_no = 4140
-# tray.AddModule("I3Reader", 
-#     filename='/data/sim/IceCube/2016/filtered/level2/CORSIKA-in-ice/20787/0000000-0000999/Level2_IC86.2016_corsika.020787.000220.i3.zst'
-#     )
-
-# run_no = 78700509
-# evt_no = 1439
-# tray.AddModule("I3Reader", 
-#     filename='/data/sim/IceCube/2016/filtered/level2/CORSIKA-in-ice/20787/0000000-0000999/Level2_IC86.2016_corsika.020787.000508.i3.zst'
-#     )
-
 import glob
-file_list = glob.glob("")
 
 run_no = 119510
 evt_no = 19598412
+file_list = glob.glob("/data/exp/IceCube/2012/filtered/level2pass2a/0130/Run00119510/*_00000050.i3.zst")
+
+run_no = 120680
+evt_no = 1605018
+file_list = glob.glob("/data/exp/IceCube/2012/filtered/level2pass2a/0916/Run00120680/*_00000004.i3.zst")
+
+run_no = 122590
+evt_no = 45181927
+file_list = glob.glob("/data/exp/IceCube/2013/filtered/level2pass2a/0624/Run00122590/*_00000121.i3.zst")
+
+
+tray = I3Tray()
+# # run_no = 78700221
+# # evt_no = 4140
+# # tray.AddModule("I3Reader", 
+# #     filename='/data/sim/IceCube/2016/filtered/level2/CORSIKA-in-ice/20787/0000000-0000999/Level2_IC86.2016_corsika.020787.000220.i3.zst'
+# #     )
+
+# # run_no = 78700509
+# # evt_no = 1439
+# # tray.AddModule("I3Reader", 
+# #     filename='/data/sim/IceCube/2016/filtered/level2/CORSIKA-in-ice/20787/0000000-0000999/Level2_IC86.2016_corsika.020787.000508.i3.zst'
+# #     )
+
+tray.AddModule("I3Reader", filenamelist=file_list )
 
 tray.AddModule(find_event, 'find_event', run = run_no, evt = evt_no,
     Streams=[icetray.I3Frame.Physics, icetray.I3Frame.DAQ])
 
-tray.AddModule("I3Writer", "wrat",
+tray.AddModule("I3Writer", "write",
     Streams=[icetray.I3Frame.Physics, icetray.I3Frame.DAQ], 
-    filename=f'{run_no}_{evt_no}.i3')
+    filename=f'{run_no}_{evt_no}.i3.zst')
 
 tray.Execute()
 tray.Finish()
-
-# tray.Add("I3Writer", filename="quick.i3.zst")
-# tray.Execute(5)
