f3da2623399b06b553b883c1b9308aff31ca56c3
Brian Clark <baclark@msu.edu>
2022-07-21 11:08:47 +0900

    update so that we also have plots of ndoms

--- studies/2022.07_cuts/L2_charge_ndoms.py
+++ studies/2022.07_cuts/L2_charge_ndoms.py
@@ -13,6 +13,9 @@ cfg_file = 'config.yaml'
 cfg_file = yaml.safe_load(open(cfg_file))
 charge_var = cfg_file['variables']['charge']['variable']
 charge_val = cfg_file['variables']['charge']['value']
+ndoms_var =  cfg_file['variables']['ndoms']['variable']
+ndoms_val =  cfg_file['variables']['ndoms']['value']
+alt_ndoms_var = 'HitMultiplicityValues'
 
 
 # set up our flux models
@@ -33,28 +36,13 @@ def astro_flux(energy):
 # juliet (EHE neutrinos)
 juliet_species = ["nue", "numu", "nutau", "mu", "tau"]
 juliet_energy_levels = ["high_energy"]
-# juliet_species = ["mu"]
+juliet_species = ["mu"]
 
 corsika_sets = ["20787"]
 
 nugen_sets = ["nue", "numu"]
 
 burn_samples = ["IC86-I-pass2", "IC86-II-pass2", "IC86-III-pass2"]
-
-
-def get_from_juliet(f, var, val):
-
-    # get the charge
-    charge = f.get_node(f'/{var}')
-    charge = charge.col(f'{val}')
-
-    # weight dict and prop matrix
-    weight_dict = f.get_node('/JulietWeightDict')
-    prop_matrix = [f.get_node(f'/PropagationMatrix{flav}') for flav in ['NuE', 'NuMu', 'NuTau']]
-
-    evts_per_file = weight_dict.col('EventsPerFile')[0]
-
-    return charge, weight_dict, prop_matrix, evts_per_file
 
 style.use('/home/brian/IceCube/ehe/max_tools/EHE_analysis/eheanalysis/ehe.mplstyle')
 
@@ -64,39 +52,56 @@ for b in burn_samples:
 print("Total livetime {}".format(livetime))
 
 charge_bins = np.logspace(2, 8, 51)
-charge_bin_centers = (charge_bins[1:] + charge_bins[:-1]) * 0.5
+charge_bin_centers = plotting.get_bin_centers(charge_bins)
+
+ndoms_bins = np.linspace(0,4000, 50)
+ndoms_bin_centers = plotting.get_bin_centers(ndoms_bins)
 
 
 #############################
 # ehe/cosmogenic flux (juliet)
 #############################
-summed_ehe = None
+summed_ehe_q = None
+summed_ehe_ndoms = None
 ehe_q_projection = np.asarray([])
+ehe_ndoms_projection = np.asarray([])
 for s in juliet_species:
     for l in juliet_energy_levels:
 
         print(f"Working on juliet {s}")
         the_f = tables.open_file(cfg_file['juliet'][s][l]['file'])
-        charge, weight_dict, prop_matrix, evts_per_file = get_from_juliet(the_f, charge_var, charge_val)
+        weight_dict, prop_matrix, evts_per_file = weighting.get_juliet_weightdict_and_propmatrix(the_f)
+        charge = the_f.get_node(f'/{charge_var}').col(f'{charge_val}')
+        ndoms = the_f.get_node(f'/{ndoms_var}').col(f'{ndoms_val}')
         n_gen = cfg_file['juliet'][s][l]['n_files'] * evts_per_file
 
-        h = weighting.make_enu_2d_hist( weight_dict, n_gen, gzk_partial,
+        h_charge = weighting.make_enu_2d_hist( weight_dict, n_gen, gzk_partial,
             var_values=charge, var_bins=charge_bins,
             prop_matrix=prop_matrix, livetime=livetime
             )
+        h_ndoms = weighting.make_enu_2d_hist( weight_dict, n_gen, gzk_partial,
+            var_values=ndoms, var_bins=ndoms_bins,
+            prop_matrix=prop_matrix, livetime=livetime
+            )
     
-        if summed_ehe is None:
-            summed_ehe = copy.deepcopy(h)
+        if summed_ehe_q is None:
+            summed_ehe_q = copy.deepcopy(h_charge)
+            summed_ehe_ndoms = copy.deepcopy(h_ndoms)
         else:
-            summed_ehe += copy.deepcopy(h)
+            summed_ehe_q += copy.deepcopy(h_charge)
+            summed_ehe_ndoms += copy.deepcopy(h_ndoms)
     
         the_f.close()
-ehe_q_projection = summed_ehe.sum(axis=1) # project down onto the charge axis
+if summed_ehe_q is not None:
+    ehe_q_projection = summed_ehe_q.sum(axis=1) # project down onto the charge axis
+if summed_ehe_ndoms is not None:
+    ehe_ndoms_projection = summed_ehe_ndoms.sum(axis=1) # project onto ndoms axis
 
 #############################
 # muon bundles (corsika)
 #############################
 cor_charge = np.asarray([])
+cor_ndoms = np.asarray([])
 cor_weights = np.asarray([])
 for c in corsika_sets:
     print("Working on corsika {}".format(c))
@@ -105,6 +110,10 @@ for c in corsika_sets:
         cfg_file['corsika'][c]['n_files']
         )
     cor_charge = np.concatenate((cor_charge, cor_weighter.get_column(charge_var, charge_val)))
+    try:
+        cor_ndoms = np.concatenate((cor_ndoms, cor_weighter.get_column(ndoms_var, ndoms_val)))
+    except:
+        cor_ndoms = np.concatenate((cor_ndoms, cor_weighter.get_column(alt_ndoms_var, ndoms_val)))
     cor_weights = np.concatenate((cor_weights, cor_weighter.get_weights(cr_flux) * livetime))
     cor_file.close()
 
@@ -113,6 +122,7 @@ for c in corsika_sets:
 # atmospheric and astrophysical neutrinos (nugen)
 #############################
 nugen_charges = np.asarray([])
+nugen_ndoms = np.asarray([])
 nugen_atmo_weights = np.asarray([])
 nugen_astro_weights = np.asarray([])
 for n in nugen_sets:
@@ -122,7 +132,12 @@ for n in nugen_sets:
         cfg_file['nugen'][n]['n_files']
         )
     this_nugen_charge = np.asarray(nugen_weighter.get_column(charge_var, charge_val))
+    try:
+        this_nugen_ndoms = np.asfarray(nugen_weighter.get_column(ndoms_var, ndoms_val))
+    except:
+        this_nugen_ndoms = np.asfarray(nugen_weighter.get_column(alt_ndoms_var, ndoms_val))
     nugen_charges = np.concatenate((nugen_charges, this_nugen_charge))
+    nugen_ndoms = np.concatenate((nugen_ndoms, this_nugen_ndoms))
     
     this_nugen_atmo_weights = nugen_weighter.get_weights(atmo_flux) * livetime
     nugen_atmo_weights = np.concatenate((nugen_atmo_weights, this_nugen_atmo_weights))
@@ -136,43 +151,86 @@ for n in nugen_sets:
 # data
 #############################
 data_charges = np.asarray([])
+data_ndoms = np.asfarray([])
 for b in burn_samples:
     print("Working on burn samples {}".format(b))
     data_file = pd.HDFStore(cfg_file['burn_sample'][b]['file'])
     this_charge = np.asarray(data_file.get(charge_var).get(charge_val))
+    try:
+        this_ndoms = np.asarray(data_file.get(ndoms_var).get(ndoms_val))
+    except:
+        this_ndoms = np.asarray(data_file.get(alt_ndoms_var).get(ndoms_val))
     data_charges = np.concatenate((data_charges, this_charge))
+    data_ndoms = np.concatenate((data_ndoms, this_ndoms))
     data_file.close()
 
-data, data_bins = np.histogram(data_charges, bins=charge_bins)
-errs = np.sqrt(data)
+data_q, data_bins_q = np.histogram(data_charges, bins=charge_bins)
+errs_q = np.sqrt(data_q)
+
+data_ndoms, data_bins_ndoms = np.histogram(data_ndoms, bins=ndoms_bins)
+errs_ndoms = np.sqrt(data_ndoms)
+
 
 #############################
 # histogram of charge
 #############################
+fig, ax = plt.subplots(1,1)
+
+# plot the MC (sum, and individual components)
+ax.hist(
+    x = np.concatenate((cor_charge, nugen_charges, nugen_charges,charge_bin_centers)),
+    weights = np.concatenate((cor_weights, nugen_atmo_weights, nugen_astro_weights, ehe_q_projection)),
+    bins = charge_bins, histtype='step', label='Sum')
+ax.hist(cor_charge, bins=charge_bins, weights=cor_weights, 
+    histtype='step', label=r'Atm $\mu$ (H4a)')
+ax.hist(nugen_charges, bins=charge_bins, weights=nugen_atmo_weights, 
+    histtype='step', label=r'Atm $\nu$ (H3a, Sibyll 2.3c)')
+ax.hist(nugen_charges, bins=charge_bins, weights=nugen_astro_weights, 
+    histtype='step', label=r'Astro $\nu$ (north tracks, $\nu_{e}$ + $\nu_{\mu}$ only)')
+ax.hist(charge_bin_centers, bins=charge_bins, weights=ehe_q_projection,
+    histtype='step', label=r'Cosmo $\nu$ (Ahlers GZK)')
+
+# plot the data
+ax.errorbar(charge_bin_centers, data_q, yerr=errs_q, fmt='ko', label='Burn Sample')
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_ylabel('Events / bin/ {:.2f} days'.format(livetime/(60*60*24)))
+ax.set_xlabel('Q / PE')
+ax.set_ylim([1E-4, 1E7])
+ax.legend()
+fig.tight_layout()
+fig.savefig('q_hist.png')
+
+
+# #############################
+# # histogram of ndoms
+# #############################
+
 fig2, ax2 = plt.subplots(1,1)
 
 # plot the MC (sum, and individual components)
 ax2.hist(
-    x = np.concatenate((cor_charge, nugen_charges, nugen_charges,charge_bin_centers)),
-    weights = np.concatenate((cor_weights, nugen_atmo_weights, nugen_astro_weights, ehe_q_projection)),
-    bins = charge_bins, histtype='step', label='Sum')
-ax2.hist(cor_charge, bins=charge_bins, weights=cor_weights, 
+    x = np.concatenate((cor_ndoms, nugen_ndoms, nugen_ndoms,ndoms_bin_centers)),
+    weights = np.concatenate((cor_weights, nugen_atmo_weights, nugen_astro_weights, ehe_ndoms_projection)),
+    bins = ndoms_bins, histtype='step', label='Sum')
+ax2.hist(cor_ndoms, bins=ndoms_bins, weights=cor_weights, 
     histtype='step', label=r'Atm $\mu$ (H4a)')
-ax2.hist(nugen_charges, bins=charge_bins, weights=nugen_atmo_weights, 
+ax2.hist(nugen_ndoms, bins=ndoms_bins, weights=nugen_atmo_weights, 
     histtype='step', label=r'Atm $\nu$ (H3a, Sibyll 2.3c)')
-ax2.hist(nugen_charges, bins=charge_bins, weights=nugen_astro_weights, 
+ax2.hist(nugen_ndoms, bins=ndoms_bins, weights=nugen_astro_weights, 
     histtype='step', label=r'Astro $\nu$ (north tracks, $\nu_{e}$ + $\nu_{\mu}$ only)')
-ax2.hist(charge_bin_centers, bins=charge_bins, weights=ehe_q_projection,
+ax2.hist(ndoms_bin_centers, bins=ndoms_bins, weights=ehe_ndoms_projection,
     histtype='step', label=r'Cosmo $\nu$ (Ahlers GZK)')
 
 # plot the data
-ax2.errorbar(charge_bin_centers, data, yerr=errs, fmt='ko', label='Burn Sample')
+ax2.errorbar(ndoms_bin_centers, data_ndoms, yerr=errs_ndoms, fmt='ko', label='Burn Sample')
 
-ax2.set_xscale('log')
+# ax2.set_xscale('log')
 ax2.set_yscale('log')
 ax2.set_ylabel('Events / bin/ {:.2f} days'.format(livetime/(60*60*24)))
-ax2.set_xlabel('Q / PE')
-ax2.set_ylim([1E-4, 1E7])
+ax2.set_xlabel('N DOMs')
+ax2.set_ylim([1E-7, 1E7])
 ax2.legend()
 fig2.tight_layout()
-fig2.savefig('q_hist.png')
+fig2.savefig('ndoms_hist.png')
