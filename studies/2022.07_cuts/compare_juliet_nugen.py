c4d384947c50e962358841f0679ed564a6ac0fce
Brian Clark <baclark@msu.edu>
2022-08-02 19:13:07 +0900

    update the juliet comparison plots

--- studies/2022.07_cuts/compare_juliet_nugen.py
+++ studies/2022.07_cuts/compare_juliet_nugen.py
@@ -17,6 +17,8 @@ cfg_file = 'config.yaml'
 cfg_file = yaml.safe_load(open(cfg_file))
 charge_var = cfg_file['variables']['charge']['variable']
 charge_val = cfg_file['variables']['charge']['value']
+czen_var = cfg_file['variables']['zenith']['variable']
+czen_val = cfg_file['variables']['zenith']['value']
 
 
 livetime = 60*60*24*365 # 1 year
@@ -31,10 +33,11 @@ def astro_flux(energy):
 juliet_weights = np.asarray([])
 juliet_edets = np.asarray([])
 juliet_charges = np.asarray([])
+juliet_czens = np.asarray([])
 
 juliet_species = ["nue", "numu", "nutau", "mu", "tau"]
 juliet_energy_levels = ["high_energy"]
-# juliet_species = ["nue"]
+juliet_species = ["nue"]
 
 
 selection = {
@@ -51,7 +54,8 @@ for s in juliet_species:
         prop_matrix = prop_matrix[selection['juliet_index']] # select only the nue prop matrix
         n_gen = cfg_file['juliet'][s][l]['n_files'] * evts_per_file
         edets = the_f.get_node("/I3JulietPrimaryParticle").col("energy")
-        q = the_f.get_node("/Homogenized_QTot").col("value")
+        q = the_f.get_node(f"/{charge_var}").col(f"{charge_val}")
+        czen = np.cos(the_f.get_node(f"/{czen_var}").col(f"{czen_val}"))
 
         weights = weighting.calc_juliet_weight_from_weight_dict_and_prop_matrix(
             weight_dict=weight_dict, prop_matrix=prop_matrix, 
@@ -62,6 +66,7 @@ for s in juliet_species:
         juliet_weights = np.concatenate((juliet_weights, weights))
         juliet_edets = np.concatenate((juliet_edets, edets))
         juliet_charges = np.concatenate((juliet_charges, q))
+        juliet_czens = np.concatenate((juliet_czens, czen))
         
         the_f.close()
 
@@ -73,14 +78,33 @@ nugen_weighter = weighting.get_weighter( nugen_file,  'nugen',
     )
 nugen_edet = nugen_weighter.get_column('PolyplopiaPrimary', 'energy')
 nugen_weights = nugen_weighter.get_weights(astro_flux) * livetime
-nugen_charges = nugen_weighter.get_column('Homogenized_QTot', 'value')
+nugen_charges = nugen_weighter.get_column(charge_var, charge_val)
+nugen_czens = np.cos(nugen_weighter.get_column(czen_var, czen_val))
 nugen_file.close()
+
+
+# energy at detector (unweighted -- just to see counts)
+fig, ax =  plt.subplots(1,1)
+bins = np.logspace(3, 9, 60)
+n_j, b_j, p_j = ax.hist(juliet_edets, bins=bins, histtype='step', label="Juliet")
+n_n, b_n, p_n = ax.hist(nugen_edet, bins=bins,  histtype='step', label="NuGen")
+ax.set_ylabel("Events / {:.2f} days".format(livetime/60/60/24))
+ax.set_xlabel(r'E$_{det}$ / GeV')
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.legend(loc='upper left')
+ax.set_title('{}'.format(selection['flavor']))
+fig.tight_layout()
+fig.savefig('plots/hist_compare_edet_unweighted.png')
+del fig, ax
+
 
 # mask energies outside their overlap energy region
 nugen_mask = (np.log10(nugen_edet) > 5) & (np.log10(nugen_edet) < 8)
 juliet_mask = (np.log10(juliet_edets) > 5) & (np.log10(juliet_edets) < 8)
 
 
+# energy at detector (weighted)
 fig, (ax, axr) =  plt.subplots(2,1, sharex=True, gridspec_kw={'height_ratios': [3,1]})
 bins = np.logspace(4, 9, 60)
 n_j, b_j, p_j = ax.hist(juliet_edets[juliet_mask], bins=bins, weights=juliet_weights[juliet_mask],
@@ -98,13 +122,14 @@ axr.plot(bin_centers, n_j/n_n - 1, 'o')
 axr.set_xlabel(r'E$_{det}$ / GeV')
 axr.set_ylabel('Juliet/NuGen - 1')
 axr.set_xscale('log')
-axr.set_ylim([-1,1])
+axr.set_ylim([-0.6,0.6])
 axr.axhline(0, linestyle='--')
 
 fig.tight_layout()
 fig.savefig('plots/hist_compare_edet.png')
 del fig, ax, axr
 
+# charge
 fig, (ax, axr) =  plt.subplots(2,1, sharex=True, gridspec_kw={'height_ratios': [3,1]})
 bins = np.logspace(2, 8, 60)
 n_j, b_j, p_j = ax.hist(juliet_charges[juliet_mask], bins=bins, weights=juliet_weights[juliet_mask],
@@ -122,9 +147,33 @@ axr.plot(bin_centers, n_j/n_n - 1, 'o')
 axr.set_xlabel(r'Charge / PE')
 axr.set_ylabel('Juliet/NuGen - 1')
 axr.set_xscale('log')
-axr.set_ylim([-1,1])
+axr.set_ylim([-0.6,0.6])
 axr.axhline(0, linestyle='--')
 
 fig.tight_layout()
 fig.savefig('plots/hist_compare_charge.png')
+del fig, ax, axr
+
+# cosine zenith
+fig, (ax, axr) =  plt.subplots(2,1, sharex=True, gridspec_kw={'height_ratios': [3,1]})
+bins = np.linspace(-1, 1, 20)
+n_j, b_j, p_j = ax.hist(juliet_czens[juliet_mask], bins=bins, weights=juliet_weights[juliet_mask],
+        histtype='step', label="Juliet")
+n_n, b_n, p_n = ax.hist(nugen_czens[nugen_mask], bins=bins, weights=nugen_weights[nugen_mask],
+        histtype='step', label="NuGen")
+ax.set_ylabel("Events / {:.2f} days".format(livetime/60/60/24))
+ax.set_yscale('log')
+ax.legend()
+ax.set_title('{}'.format(selection['flavor']))
+plt.setp(ax.get_xticklabels(), visible=False)
+
+bin_centers = plotting.get_bin_centers(bins)
+axr.plot(bin_centers, n_j/n_n - 1, 'o')
+axr.set_xlabel(r'cos($\theta$)')
+axr.set_ylabel('Juliet/NuGen - 1')
+axr.set_ylim([-0.6,0.6])
+axr.axhline(0, linestyle='--')
+
+fig.tight_layout()
+fig.savefig('plots/hist_compare_czen.png')
 del fig, ax, axr
--- studies/2022.07_cuts/unify_hdf5_naming.py
+++ studies/2022.07_cuts/unify_hdf5_naming.py
@@ -10,19 +10,20 @@ import h5py
 top_dir = "/disk20/users/brian/IceCube/unified_naming/original/"
 
 files_to_fix = [
-    "combo_20787.hdf5",
-    "combo_20787_smaller.hdf5",
-    "combo_21218.hdf5",
-    "combo_21220.hdf5",
-    "combo_IC86-III-pass2_L4.hdf5",
-    "combo_IC86-II-pass2_L4.hdf5",
-    "combo_IC86-I-pass2_L4.hdf5",
-    "Level2_prime_00000001.hd5",
-    "mu_high_energy_l2_prime_1k_merged.hd5",
-    "nue_high_energy_l2_prime_1k_merged.hd5",
-    "numu_high_energy_l2_prime_1k_merged.hd5",
-    "nutau_high_energy_l2_prime_1k_merged.hd5",
-    "tau_high_energy_l2_prime_1k_merged.hd5"
+    # "combo_20787.hdf5",
+    # "combo_20787_smaller.hdf5",
+    # "combo_21218.hdf5",
+    # "combo_21220.hdf5",
+    # "combo_IC86-III-pass2_L4.hdf5",
+    # "combo_IC86-II-pass2_L4.hdf5",
+    # "combo_IC86-I-pass2_L4.hdf5",
+    # "Level2_prime_00000001.hd5",
+    # "mu_high_energy_l2_prime_1k_merged.hd5",
+    # "nue_high_energy_l2_prime_1k_merged.hd5",
+    # "numu_high_energy_l2_prime_1k_merged.hd5",
+    # "nutau_high_energy_l2_prime_1k_merged.hd5",
+    # "tau_high_energy_l2_prime_1k_merged.hd5"
+    "combo_v2_21218.hdf5"
 ]
 
 for f_to_fix in files_to_fix:
