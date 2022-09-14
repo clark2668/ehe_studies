1e9b1b87d435098c34dec8489608ef933668f1a9
Brian Clark <baclark@msu.edu>
2022-09-15 03:43:21 +0900

    quick update to unify naming code

--- studies/2022.07_cuts/unify_hdf5_naming.py
+++ studies/2022.07_cuts/unify_hdf5_naming.py
@@ -4,27 +4,12 @@ The point of this script is to unify the naming conventions in the hdf5 file,
 so that the hit multiplicity and linefit variables all have the same name for uniform handling.
 """
 
-
 import h5py
 
-top_dir = "/disk20/users/brian/IceCube/unified_naming/original/"
+top_dir = "/disk20/users/brian/IceCube/corsika/"
 
 files_to_fix = [
-    # "combo_20787.hdf5",
-    # "combo_20787_smaller.hdf5",
-    # "combo_21218.hdf5",
-    # "combo_21220.hdf5",
-    # "combo_IC86-III-pass2_L4.hdf5",
-    # "combo_IC86-II-pass2_L4.hdf5",
-    # "combo_IC86-I-pass2_L4.hdf5",
-    # "Level2_prime_00000001.hd5",
-    # "mu_high_energy_l2_prime_1k_merged.hd5",
-    # "nue_high_energy_l2_prime_1k_merged.hd5",
-    # "numu_high_energy_l2_prime_1k_merged.hd5",
-    # "nutau_high_energy_l2_prime_1k_merged.hd5",
-    # "tau_high_energy_l2_prime_1k_merged.hd5"
-    # "combo_v2_21218.hdf5"
-    "nue_high_energy_l2_prime_1k_merged_csms.hd5"
+    "combo_20787.hdf5",
 ]
 
 for f_to_fix in files_to_fix:
@@ -36,21 +21,21 @@ for f_to_fix in files_to_fix:
         top_keys = [key for key in f.keys()]
         
         # fix the linefit name
-        if "EHELineFit" in top_keys:
+        if "EHELineFit" in top_keys and 'LineFit' not in top_keys:
             print("    Fixing EHELineFit")
-            f["LineFit_unified"] = f["EHELineFit"]
-        elif "LineFit_redo" in top_keys:
+            f["LineFit"] = f["EHELineFit"]
+        elif "LineFit_redo" in top_keys and 'LineFit' not in top_keys:
             print("    Fixing LineFit_redo")
-            f["LineFit_unified"] = f["LineFit_redo"]
-        elif "LineFit" in top_keys:
-            print("    Fixing LineFit")
-            f["LineFit_unified"] = f["LineFit"]
+            f["LineFit"] = f["LineFit_redo"]
+        # elif "LineFit" in top_keys:
+            # print("    Fixing LineFit")
+            # f["LineFit_unified"] = f["LineFit"]
         
         # now, fix the hit multiplicity label
-        if "HitMultiplicityValues" in top_keys:
+        if "HitMultiplicityValues" in top_keys and 'CVMultiplicity' not in top_keys:
             print("    Fixing HitMultiplicityValues")
-            f["HitMultiplicityValues_unified"] = f["HitMultiplicityValues"]
-        elif "CVMultiplicity" in top_keys:
-            print("    Fixing CVMultiplicity")
-            f["HitMultiplicityValues_unified"] = f["CVMultiplicity"]            
+            f["CVMultiplicity"] = f["HitMultiplicityValues"]
+        # elif "CVMultiplicity" in top_keys and
+            # print("    Fixing CVMultiplicity")
+            # f["HitMultiplicityValues_unified"] = f["CVMultiplicity"]            
         
