1b83018434c331cc4bc4556cfd412353b95c95ef
Brian Clark <baclark@msu.edu>
2022-09-16 10:01:11 +0900

    update new cut design plots for collab meeting

--- studies/2022.07_cuts/design_new_speed_cut.py
+++ studies/2022.07_cuts/design_new_speed_cut.py
@@ -14,13 +14,15 @@ import pandas as pd
 import copy
 import yaml
 
-from eheanalysis import plotting, analysis_9yr
+from eheanalysis import plotting, analysis_9yr, cuts
 
 cfg_file = 'config.yaml'
 cfg_file = yaml.safe_load(open(cfg_file))
 
+do_efficiency = True
+do_plots = True
 version = 'old'
-# version = "new"
+version = 'new'
 if version == 'new':
     charge_var = cfg_file['variables']['charge']['variable']
     charge_val = cfg_file['variables']['charge']['value']
@@ -28,7 +30,7 @@ if version == 'new':
     ndoms_val =  cfg_file['variables']['ndoms']['value']
     speed_var = cfg_file['variables']['speed']['variable']
     speed_val = cfg_file['variables']['speed']['value']
-    speed_bins = np.linspace(0, 2, 601)
+    speed_bins = np.linspace(0, 2, 251)
     speed_label = "LineFit Speed"
     speed_cut = 0.27
     speed_lims = [0, 0.5]
@@ -42,13 +44,13 @@ elif version == 'old':
     speed_var = "EHEOpheliaSRT_ImpLF"
     speed_val = "fitQuality"
     speed_label = "Ophelia FitQual"
-    speed_bins = np.linspace(0, 1000, 601)
+    speed_bins = np.linspace(0, 1000, 151)
     speed_cut = 100
     speed_lims = [0,500]
     log10_q_cut = np.log10(25000)
     nue_cumulative_sign = 1
 
-charge_bins = np.logspace(4, 7, 16)
+charge_bins = np.logspace(4, 7, 31)
 charge_bin_centers = plotting.get_bin_centers(charge_bins)
 speed_bin_centers = plotting.get_bin_centers(speed_bins)
 q_cut = np.power(10., log10_q_cut)
@@ -60,7 +62,7 @@ from eheanalysis import weighting
 gzk_flux = fluxes.EHEFlux("ahlers_gzk")
 gzk_partial = partial(gzk_flux, which_species="nue_sum") 
 cr_flux = weighting.get_flux_model('GaisserH4a', 'corsika')
-atmo_flux = weighting.get_flux_model('H3a_SIBYLL23C_pr', 'nugen')
+atmo_flux = weighting.get_flux_model('H3a_SIBYLL23C', 'nugen')
 
 burn_samples = ["IC86-I-pass2", "IC86-II-pass2", "IC86-III-pass2"]
 
@@ -108,9 +110,11 @@ for c in corsika_sets:
 nugen_atmo_weights = np.asarray([])
 nugen_charge = np.asarray([])
 nugen_speed = np.asarray([])
+nugen_energy = np.asarray([])
 
 nugen_sets = ["nue", "numu", "nutau"]
 # nugen_sets = ["nue"]
+# nugen_sets = []
 for n in nugen_sets:
     print("Working on nugen {}".format(n))
 
@@ -121,6 +125,7 @@ for n in nugen_sets:
     this_nugen_charge = np.asarray(nugen_weighter.get_column(charge_var, charge_val))    
     this_nugen_ndoms = np.asarray(nugen_weighter.get_column(ndoms_var, ndoms_val))
     this_nugen_speed = nugen_weighter.get_column(speed_var, speed_val)
+    this_nugen_energy = nugen_weighter.get_column('PolyplopiaPrimary', 'energy')
     
     L2_q_mask = this_nugen_charge > q_cut
     L2_ndom_mask = this_nugen_ndoms > ndom_cut
@@ -131,7 +136,8 @@ for n in nugen_sets:
     
     this_nugen_atmo_weights = nugen_weighter.get_weights(atmo_flux) * livetime
     nugen_atmo_weights = np.concatenate((nugen_atmo_weights, this_nugen_atmo_weights[L2_mask]))
-
+    nugen_energy = np.concatenate((nugen_energy, this_nugen_energy[L2_mask]))
+    
     nugen_file.close()
 
 summed_nugen_weight = np.sum(nugen_atmo_weights)
@@ -148,8 +154,13 @@ ehe_L2_mask = np.asarray([])
 
 juliet_species = ["nue", "numu", "nutau", "mu", "tau"]
 juliet_energy_levels = ["high_energy", "very_high_energy"]
-juliet_species = ["numu"]
-juliet_energy_levels = ["high_energy"]
+# juliet_species = ["numu"]
+# juliet_energy_levels = ["high_energy"]
+# juliet_species = []
+
+ehe_energy_weights_nocuts = None
+ehe_energy_weights_after_L2 = None
+ehe_energy_weights_after_L3 = None
 
 for s in juliet_species:
     for l in juliet_energy_levels:
@@ -172,12 +183,88 @@ for s in juliet_species:
             L2_q_mask = charge > q_cut
             L2_ndoms_mask = ndoms > ndom_cut
             L2_mask = L2_q_mask & L2_ndoms_mask
-
+            
             ehe_weights = np.concatenate((ehe_weights, weights[L2_mask]))
             ehe_charge = np.concatenate((ehe_charge, charge[L2_mask]))
             ehe_speed = np.concatenate((ehe_speed, speed[L2_mask]))
-    
+            
+            # compute the product of the L2 and L3 mask
+            if version=="old":
+                L3_mask = analysis_9yr.track_quality_cut_pass_9yr(speed, charge)
+            elif version=="new":
+                L3_mask = cuts.track_quality_cut(speed, charge)
+        
+            if do_efficiency:
+                # if we want to tabulate efficiencies vs energy
+                # then we need to project into enu space
+
+                # no cuts
+                the_enu_weights = weighting.calc_juliet_flux_weight(
+                    weight_dict = weight_dict, prop_matrix = prop_matrix, 
+                    flux= gzk_partial, n_gen = n_gen, livetime=livetime,
+                )
+                if ehe_energy_weights_nocuts is None:
+                    ehe_energy_weights_nocuts = copy.deepcopy(the_enu_weights)
+                else:
+                    ehe_energy_weights_nocuts += copy.deepcopy(the_enu_weights)
+                    
+                # after L2 cuts
+                the_enu_weights_L2 = weighting.calc_juliet_flux_weight(
+                    weight_dict = weight_dict, prop_matrix = prop_matrix, 
+                    flux= gzk_partial, n_gen = n_gen, livetime=livetime,
+                    selection_mask = L2_mask
+                )
+                if ehe_energy_weights_after_L2 is None:
+                    ehe_energy_weights_after_L2 = copy.deepcopy(the_enu_weights_L2)
+                else:
+                    ehe_energy_weights_after_L2 += copy.deepcopy(the_enu_weights_L2)                
+
+                # after L3 cuts
+                
+                L3_mask = np.logical_and(L2_mask, L3_mask)
+
+                the_enu_weights_L3 = weighting.calc_juliet_flux_weight(
+                    weight_dict = weight_dict, prop_matrix = prop_matrix, 
+                    flux= gzk_partial, n_gen = n_gen, livetime=livetime,
+                    selection_mask = L3_mask
+                )
+                if ehe_energy_weights_after_L3 is None:
+                    ehe_energy_weights_after_L3 = copy.deepcopy(the_enu_weights_L3)
+                else:
+                    ehe_energy_weights_after_L3 += copy.deepcopy(the_enu_weights_L3)
+
         the_f.close()
+
+if do_efficiency:
+
+    # eff vs energy    
+    energy_bins, energy_bin_centers = weighting.get_juliet_enu_binning()
+
+    total_ehe_weight_nocuts = ehe_energy_weights_nocuts.sum()
+    total_ehe_weight_L2 = ehe_energy_weights_after_L2.sum()
+    total_ehe_weight_L3 = ehe_energy_weights_after_L3.sum()
+    total_eff_L2 = total_ehe_weight_L2/total_ehe_weight_nocuts
+    total_eff_L3 = total_ehe_weight_L3/total_ehe_weight_nocuts
+
+    fig, ax = plt.subplots(1, 1, figsize=(5,5))
+
+    # eff vs energy
+    ax.plot(energy_bin_centers, ehe_energy_weights_after_L2/ehe_energy_weights_nocuts,
+        linewidth=3, label="After L2, {:.2f}% Eff".format(total_eff_L2*100.))
+    ax.plot(energy_bin_centers, ehe_energy_weights_after_L3/ehe_energy_weights_nocuts,
+        linewidth=3, ls="--", label="After L3, {:.2f}% Eff".format(total_eff_L3*100.))
+    
+    print("Total L2 Eff {:.2f}".format(total_eff_L2*100.))
+    print("Total L3 Eff {:.2f}".format(total_eff_L3*100.))
+    ax.set_xscale('log')
+    ax.set_xlabel(r"E$_{\nu}$ / GeV")
+    ax.set_ylabel("Efficiency")
+    ax.set_ylim([0, 1.1])
+    ax.legend()
+
+    fig.tight_layout()
+    fig.savefig("plots/eff_L2_L3_{}.png".format(version), dpi=300)
+    del fig, ax
 
 
 #############################
@@ -185,8 +272,7 @@ for s in juliet_species:
 # to see how our pivot point works out
 #############################
 
-do_speed_hist_2d = True
-if do_speed_hist_2d:
+if do_plots:
         
     def get_track_quality_cut_9yr(fit_quality):
         if fit_quality <= 80:
@@ -219,7 +305,33 @@ if do_speed_hist_2d:
         print("L3 Pass rate corsika {}".format(np.sum(cor_weights[pass_mask_cor])))
         print("L3 Pass rate nugen {}".format(np.sum(nugen_atmo_weights[pass_mask_nugen])))
         print("L3 Pass rate ehe {}".format(np.sum(ehe_weights[pass_mask_ehe])))
+    if version == "new":
+        pass_mask_cor = cuts.track_quality_cut(cor_speed, cor_charge)
+        pass_mask_nugen = cuts.track_quality_cut(nugen_speed, nugen_charge)
+        pass_mask_ehe = cuts.track_quality_cut(ehe_speed, ehe_charge)
+        print("L3 Pass rate corsika {}".format(np.sum(cor_weights[pass_mask_cor])))
+        print("L3 Pass rate nugen {}".format(np.sum(nugen_atmo_weights[pass_mask_nugen])))
+        print("L3 Pass rate ehe {}".format(np.sum(ehe_weights[pass_mask_ehe])))
 
+    # energy distrbution of nugen events
+    fig, ax = plt.subplots(1, 1, figsize=(7,5))
+    bins = np.logspace(4, 9, 21)
+    ax.hist(nugen_energy, bins=bins, weights=nugen_atmo_weights,
+        linewidth=3, histtype='step', 
+        label="After L2, {:.1e} evts".format(np.sum(nugen_atmo_weights))
+    )
+    ax.hist(nugen_energy[pass_mask_nugen], bins=bins, weights=nugen_atmo_weights[pass_mask_nugen],
+        linewidth=3, histtype='step', linestyle='--',
+        label="After L3, {:.1e} evts".format(np.sum(nugen_atmo_weights[pass_mask_nugen]))
+    )
+    ax.set_ylabel("Weighted Atm Nu Events, {:.2f} days".format(livetime/60/60/24))
+    ax.set_xlabel("Neutrino Energy")
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_ylim([1E-6, 1E-1])
+    ax.legend()
+    fig.savefig("num_events_atmo_nu_L3_{}.png".format(version), dpi=300)
+    del fig, ax
         
     plotting_options = {
         'xlabel': r'LineFit Speed',
@@ -238,7 +350,7 @@ if do_speed_hist_2d:
         weights = cor_weights, bins = [speed_bins, charge_bins],
         cmap = plotting_options['cmap'], norm=colors.LogNorm()
     )
-    ax1.set_title("Atm Nu (Corsika)")
+    ax1.set_title("Atm Muon (Corsika)")
     cor_cbar = plt.colorbar(cor_im, ax=ax1)
     
     # nugen
