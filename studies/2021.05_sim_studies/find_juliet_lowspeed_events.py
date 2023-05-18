b99642da58997ea37033ebdb718afe1d47a26d26
Brian Clark <baclark@msu.edu>
2023-05-18 10:43:07 +0900

    minor update to the low speed finder

--- studies/2021.05_sim_studies/find_juliet_lowspeed_events.py
+++ studies/2021.05_sim_studies/find_juliet_lowspeed_events.py
@@ -8,7 +8,8 @@ def find_event_2(frame):
     if frame.Has('LineFit'):
         lfvalues = frame.Get("LineFit")
         speed = lfvalues.speed
-        if (speed < 0.25) and (speed > 0.2):
+        # if (speed < 0.25) and (speed > 0.2):
+        if speed < 0.15:
             keeper = True
     
     return keeper
@@ -36,13 +37,16 @@ tray = I3Tray()
 
 filenamelist = []
 # filenamelist.append('/cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz')
-filenamelist.append('/disk19/users/mmeier/simulations/table_based_sim/juliet/mu/high_energy/L2/1/Level2_00000999.i3.zst')
+# filenamelist.append('/disk19/users/mmeier/simulations/table_based_sim/juliet/mu/high_energy/L2/1/Level2_00000999.i3.zst')
 
 # filedir = "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21220/0000000-0000999/"
-# from glob import glob
+filedir = "/disk19/users/mmeier/simulations/table_based_sim/juliet/mu/high_energy/L2/1/"
+from glob import glob
 # files = sorted(glob(filedir + "Level2_IC86.2016_NuMu.021220.0000*.i3.zst"))
-# for f in files:
-#     filenamelist.append(f)
+files = sorted(glob(filedir + "Level2_0000051*.i3.zst"))
+for f in files:
+    filenamelist.append(f)
+print(filenamelist)
 
 tray = I3Tray()
 tray.AddModule("I3Reader", filenamelist=filenamelist)
@@ -60,7 +64,7 @@ tray.AddModule(find_event_2, 'find_event',
 tray.AddModule("I3Writer", "wrat",
     Streams=[icetray.I3Frame.Physics, icetray.I3Frame.DAQ], 
     DropOrphanStreams=[icetray.I3Frame.Calibration, icetray.I3Frame.DAQ],
-    filename=f'juliet_mu_lowspeed.i3.zst')
+    filename=f'juliet_mu_verylowspeed.i3.zst')
 
 tray.Execute()
 tray.Finish()
