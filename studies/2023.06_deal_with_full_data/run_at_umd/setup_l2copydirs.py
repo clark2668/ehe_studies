6aa1d64364e0e5bf499a5299fa6f21dfb8f8aa06
clark2668 <baclark@msu.edu>
2023-06-17 22:36:06 -0500

    update the code that  makes l2 datawarehouse copy dirs so that it gets the 2017 calendar year split between level2 and level2pass2a right

--- studies/2023.06_deal_with_full_data/run_at_umd/setup_l2copydirs.py
+++ studies/2023.06_deal_with_full_data/run_at_umd/setup_l2copydirs.py
@@ -19,13 +19,33 @@ do_setup_year_folders = False
 if do_setup_year_folders:
     years = range(2010,2023,1)
     for y in years:
+        
+        if y < 2017:
             
-        # make the diretory structure to mirror madison
-        pathlib.Path(
-            os.path.join(top_dir,str(y),'filtered','level2pass2a')
-            ).mkdir(parents=True, exist_ok=True)
+            # make the diretory structure to mirror madison
+            pathlib.Path(
+                os.path.join(top_dir,str(y),'filtered','level2pass2a')
+                ).mkdir(parents=True, exist_ok=True)
+        
+        elif y > 2017:
+            
+            # make the diretory structure to mirror madison
+            pathlib.Path(
+                os.path.join(top_dir,str(y),'filtered','level2')
+                ).mkdir(parents=True, exist_ok=True)
+        
+        elif y == 2017:
+            
+            pathlib.Path(
+                os.path.join(top_dir,str(y),'filtered','level2pass2a')
+                ).mkdir(parents=True, exist_ok=True)
 
-do_setup_run_folders = True
+            pathlib.Path(
+                os.path.join(top_dir,str(y),'filtered','level2')
+                ).mkdir(parents=True, exist_ok=True)
+        
+
+do_setup_run_folders = False
 if do_setup_run_folders:
     
     # now 
@@ -35,6 +55,19 @@ if do_setup_run_folders:
     run_folders = stuff['run_folders']
     year_folders = stuff['year_folders']
     for m, r, y in zip(month_folders, run_folders, year_folders):
-        pathlib.Path(
-            os.path.join(top_dir,str(y),'filtered','level2pass2a',m,r)
-        ).mkdir(parents=True, exist_ok=True)
+
+        the_run_no = int(r.split('Run00')[1])
+        
+        if the_run_no < 129523:
+        
+            print(f"Run {the_run_no}, use level2pass2a")
+            pathlib.Path(
+                os.path.join(top_dir,str(y),'filtered','level2pass2a',m,r)
+            ).mkdir(parents=True, exist_ok=True)
+        
+        else:
+            print(f"Run {the_run_no}, use level2")
+            pathlib.Path(
+                os.path.join(top_dir,str(y),'filtered','level2',m,r)
+            ).mkdir(parents=True, exist_ok=True)
+            
