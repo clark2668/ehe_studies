c8761e2842d52758367c7cea67091f9e269e69fe
clark2668 <baclark@msu.edu>
2023-06-19 10:15:57 -0500

    update to highq multi

--- studies/2023.06_deal_with_full_data/run_at_umd/job.sh
+++ studies/2023.06_deal_with_full_data/run_at_umd/job.sh
@@ -8,5 +8,5 @@ env_path=$SROOT/metaprojects/icetray/v1.5.1/env-shell.sh
 # script_path=/data/i3home/baclark/IceCube/ehe/ehe_studies/studies/2023.06_deal_with_full_data/run_at_umd
 script_path=/home/brianclark/IceCube/ehe_studies/studies/2023.06_deal_with_full_data/run_at_umd
 
-# $env_path $script_path/highq_multi.py -i $input_file
-$env_path $script_path/highq.py -i $input_file
+$env_path $script_path/highq_multi.py -i $input_file
+# $env_path $script_path/highq.py -i $input_file
