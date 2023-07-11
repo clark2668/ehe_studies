b221931bfe7ba51b01f82371743bd77d6437f764
Brian Clark <baclark@umd.edu>
2023-07-10 21:07:39 -0400

    add folder for career plots

---
+++ other/plots_2023_career/do_stuff.sh
@@ -0,0 +1,15 @@
+
+# top_dir=/data/i3store/users/baclark/juliet/level5/
+
+# hdfwriter-merge $top_dir/nue_high_energy/1/*.hd5 -o $top_dir/nue_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/numu_high_energy/1/*.hd5 -o $top_dir/numu_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/nutau_high_energy/1/*.hd5 -o $top_dir/nutau_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/mu_high_energy/1/*.hd5 -o $top_dir/mu_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/tau_high_energy/1/*.hd5 -o $top_dir/tau_high_energy_merged_1kfiles.hdf5
+
+# hdfwriter-merge $top_dir/nue_very_high_energy/1/*.hd5 -o $top_dir/nue_very_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/numu_very_high_energy/1/*.hd5 -o $top_dir/numu_very_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/nutau_very_high_energy/1/*.hd5 -o $top_dir/nutau_very_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/mu_very_high_energy/1/*.hd5 -o $top_dir/mu_very_high_energy_merged_1kfiles.hdf5
+# hdfwriter-merge $top_dir/tau_very_high_energy/1/*.hd5 -o $top_dir/tau_very_high_energy_merged_1kfiles.hdf5
+
